8a305a7d6f948dc4c20863c76a6f63dbf5598d5a
Arnel Marcelo <arnelmarcelo@hotmail.com>
2020-05-26 10:32:43 +0300

    Update ISPDATA

--- IspDataDb/dbo/Tables/CkdOiItem.sql
+++ IspDataDb/dbo/Tables/CkdOiItem.sql
@@ -1,12 +1,14 @@
 ﻿CREATE TABLE [dbo].[CkdOiItem] (
-    [IdNo]            INT   NOT NULL,
-    [CkdIdNo]         INT   NOT NULL,
-    [JournalItemIdNo] INT   NOT NULL,
-    [Sequence]        INT   NOT NULL,
-    [Amount]          MONEY CONSTRAINT [DF_CkdOiItem_Amount] DEFAULT ((0)) NOT NULL,
-    [DiscountTaken]   MONEY CONSTRAINT [DF_CkdOiItem_DiscountTaken] DEFAULT ((0)) NOT NULL,
+    [IdNo]              INT   NOT NULL,
+    [CkdIdNo]           INT   NOT NULL,
+    [ApOpenInvoiceIdNo] INT   NOT NULL,
+    [Sequence]          INT   NOT NULL,
+    [Amount]            MONEY CONSTRAINT [DF_CkdOiItem_Amount] DEFAULT ((0)) NOT NULL,
+    [DiscountTaken]     MONEY CONSTRAINT [DF_CkdOiItem_DiscountTaken] DEFAULT ((0)) NOT NULL,
     CONSTRAINT [PK_CkdOiItemIdNo] PRIMARY KEY CLUSTERED ([IdNo] ASC)
 );
+
+
 
 
 
