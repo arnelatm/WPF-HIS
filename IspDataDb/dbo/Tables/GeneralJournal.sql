c82f33af689d083dd56e63c1fc1d0ad34590cb25
Arnel Marcelo <arnelmarcelo@hotmail.com>
2020-06-23 11:16:48 +0300

    Updated Tables And Views

--- IspDataDb/dbo/Tables/GeneralJournal.sql
+++ IspDataDb/dbo/Tables/GeneralJournal.sql
@@ -3,12 +3,15 @@
     [TransactionDate] DATE           NOT NULL,
     [ReferenceNo]     NVARCHAR (10)  COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
     [Notes]           NVARCHAR (100) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-    [Posted]          NCHAR (10)     COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+    [Posted]          BIT            NULL,
+    [ClosingJournal]  BIT            NULL,
     [Cancelled]       BIT            NULL,
     [DateCreated]     DATETIME       CONSTRAINT [DF_GeneralJournal_DateCreated] DEFAULT (getdate()) NULL,
     [DateTimeStamp]   ROWVERSION     NULL,
     CONSTRAINT [PK_JournalIdNo] PRIMARY KEY CLUSTERED ([IdNo] ASC)
 );
+
+
 
 
 
