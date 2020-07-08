d88ba281fa7eb3b15f15723ca67cf6ae1a86aff8
Arnel Marcelo <arnelmarcelo@hotmail.com>
2020-07-08 03:00:46 +0300

    Updated Databases Schema

--- IspDataDb/dbo/Tables/Chart.sql
+++ IspDataDb/dbo/Tables/Chart.sql
@@ -1,5 +1,5 @@
 ﻿CREATE TABLE [dbo].[Chart] (
-    [IDNo]               INT           IDENTITY (1, 1) NOT NULL,
+    [IdNo]               INT           NOT NULL,
     [ParentIDNo]         INT           NULL,
     [AccountCode]        VARCHAR (5)   COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
     [AccountName]        VARCHAR (50)  COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
@@ -19,14 +19,17 @@
     [IncomeExpSummary]   BIT           NULL,
     [Active]             BIT           NULL,
     [SpecialAccount]     CHAR (2)      COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+    [GroupSortOrder]     INT           NULL,
     [CreateDate]         DATETIME2 (7) NULL,
     [DateTimeStamp]      ROWVERSION    NULL,
-    CONSTRAINT [PK__ChartIdNo] PRIMARY KEY CLUSTERED ([IDNo] ASC),
-    CONSTRAINT [FK__Chart__ParentId] FOREIGN KEY ([ParentIDNo]) REFERENCES [dbo].[Chart] ([IDNo]),
+    CONSTRAINT [PK__ChartIdNo] PRIMARY KEY CLUSTERED ([IdNo] ASC),
+    CONSTRAINT [FK__Chart__ParentId] FOREIGN KEY ([ParentIDNo]) REFERENCES [dbo].[Chart] ([IdNo]),
     CONSTRAINT [IX_ChartCode] UNIQUE NONCLUSTERED ([AccountCode] ASC),
     CONSTRAINT [IX_ChartName] UNIQUE NONCLUSTERED ([AccountName] ASC),
     CONSTRAINT [IX_ChartNameAra] UNIQUE NONCLUSTERED ([AccountNameAra] ASC)
 );
+
+
 
 
 
---
+++ IspDataDb/dbo/Views/GLBalanceSheet_View.sql
@@ -0,0 +1,10 @@
+﻿
+
+
+
+
+CREATE VIEW [dbo].[GLBalanceSheet_View]
+AS
+Select idno,sum(Debit-Credit) as 'Balance',TransactionDate,ClosingJournal,Posted
+from GeneralLedger_View as Gl
+group by idno,TransactionDate,ClosingJournal,Posted
