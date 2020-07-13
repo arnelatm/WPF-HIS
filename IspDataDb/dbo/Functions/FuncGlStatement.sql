505150ef8f8fa4004ca9fd236af44a79fb05f620
Arnel Marcelo <arnelmarcelo@hotmail.com>
2020-07-13 06:25:20 +0300

    Update Ispdata Schema

--- IspDataDb/dbo/Functions/FuncGlStatement.sql
+++ IspDataDb/dbo/Functions/FuncGlStatement.sql
@@ -8,7 +8,16 @@ RETURN
 (   SELECT idno, Sum(Balance) as 'Balance'
     FROM [GLBalanceSheet_View]
 	/***WHERE (TransactionDate >= @StartDate and TransactionDate <= @EndDate ) ***/
-    WHERE (TransactionDate >= @StartDate and TransactionDate < @EndDate and  closingjournal = 0) 
-		   OR (TransactionDate >= @LastFiscalYearEnd and TransactionDate < @EndDate and  closingjournal = 1) 
+    WHERE (TransactionDate >= @StartDate and TransactionDate < @EndDate and  closingjournal = 0 and (SpecialAccount <> 'BI' or SpecialAccount <> 'EI')) 
+		   OR 
+		  (TransactionDate >= @LastFiscalYearEnd and TransactionDate < @EndDate and  closingjournal = 1 and (SpecialAccount <> 'BI' or SpecialAccount <> 'EI')) 
+		   OR
+		  (Month(TransactionDate) = Month(@StartDate) and Year(TransactionDate) = Year(@StartDate) and  closingjournal = 0 and SpecialAccount = 'BI')
+		   OR 
+		  (Month(TransactionDate) = Month(@EndDate) and Year(TransactionDate) = Year(@EndDate) and  closingjournal = 0 and SpecialAccount = 'EI')
+		   OR
+		  (Month(TransactionDate) = Month(@StartDate) and Year(TransactionDate) = Year(@StartDate) and  closingjournal = 1 and SpecialAccount = 'BI')
+		   OR
+		  (Month(TransactionDate) = Month(@EndDate) and Year(TransactionDate) = Year(@EndDate) and  closingjournal = 1 and SpecialAccount = 'EI')
 	Group By idNo
 )
--- IspDataDb/dbo/Views/IncomeStatementLayout_View.sql
+++ IspDataDb/dbo/Views/IncomeStatementLayout_View.sql
@@ -3,18 +3,18 @@
 
 
 
+
 /****** Script for SelectTopNRows command from SSMS  ******/
-  CREATE VIEW [dbo].[BalanceSheetLayout_View] as
+  CREATE VIEW [dbo].[IncomeStatementLayout_View] as
   (SELECT IdNo,ParentIDNo,AccountCode,AccountName,AccountNameAra,DetailAccount,AccountGroup,ByDebit,BYCredit,Debit,Credit,NormalBalance,CloseDebit,CloseCredit,PayeeType,
-		  WithReconciliation,IncomeExpSummary,SpecialAccount,Active,LevelNumber,Path,SortKey,
-		  CASE WHEN AccountGroup='A' THEN 1 WHEN AccountGroup='L' THEN 2 WHEN AccountGroup='E' THEN 3 END AS 'AccountGroupSort'
-		  FROM Chart_View)
+		  WithReconciliation,IncomeExpSummary,SpecialAccount,Active,LevelNumber,Path,SortKey
+		  FROM ChartIs_View)
   UNION
   (SELECT [IdNo]
       ,[ParentIdNo]
       ,'XXX' AS 'AccountCode'
       ,'Total '+ [AccountName] AS 'AccountName'
-      ,' مجموع '   + [AccountNameAra] AS 'AccountNameAra'
+      ,'مجموع' + [AccountNameAra] AS 'AccountNameAra'
       ,'1' AS 'DetailAccount'
       ,[AccountGroup]
 	  ,0
@@ -32,5 +32,4 @@
       ,[LevelNumber]+1 AS 'LevelNumber'
       ,[path]+'-A'
 	  ,REPLACE(RTRIM(REPLACE(SortKey, '0', ' ')), ' ', '0')+'999'
-	  ,CASE WHEN AccountGroup='A' THEN 1 WHEN AccountGroup='L' THEN 2 WHEN AccountGroup='E' THEN 3 END 
   FROM [ISPDATA].[dbo].[Chart_View] WHERE NOT DetailAccount=1)
