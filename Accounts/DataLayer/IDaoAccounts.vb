2b3b227c5136f31401b15149981eed86e617be1e
Arnel Marcelo <arnelmarcelo@hotmail.com>
2020-08-11 10:05:22 +0300

    editing customerentry

--- Accounts/DataLayer/IDaoAccounts.vb
+++ Accounts/DataLayer/IDaoAccounts.vb
@@ -18,12 +18,20 @@ Namespace DataLayer
 
     'End Interface
 
+    Public Interface IDaoPayees(Of TBiz)
+
+        Function UpdateOpeningBalance(ByRef bizObj As TBiz) As Integer
+        
+    End Interface
+
+    
     Public Interface IDaoJournals(Of TBiz)
 
         Function UpdateGlReferenceNumber(ByRef bizObj As TBiz) As Integer
 
     End Interface
 
+    
     Public Interface IDaoOpenInvoice(Of TBiz)
 
         Function AddRecord(ByRef openInvoice As TBiz) As Integer
--- Accounts/PresentationLayer/Forms/CustomerEntryTv.vb
+++ Accounts/PresentationLayer/Forms/CustomerEntryTv.vb
@@ -442,6 +442,7 @@ Namespace PresentationLayer.Forms
             End If
         End Sub
 
+
     End Class
 
 End Namespace
