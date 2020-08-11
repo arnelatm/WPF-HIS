e35e4e18ad61362263465debbbb590c1e8d42f00
Arnel Marcelo <arnelmarcelo@hotmail.com>
2020-08-11 10:34:30 +0300

    update supplier.sql

---
+++ IspDataDb/dbo/Tables/DefaultAccounts.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[DefaultAccounts] (
+    [IdNo]           INT      IDENTITY (1, 1) NOT NULL,
+    [AccountIdNo]    INT      NULL,
+    [SpecialAccount] CHAR (2) NULL
+);
+
+
+GO
+CREATE UNIQUE NONCLUSTERED INDEX [IX_DefaultAccountsSpecialAccount]
+    ON [dbo].[DefaultAccounts]([SpecialAccount] ASC);
+
