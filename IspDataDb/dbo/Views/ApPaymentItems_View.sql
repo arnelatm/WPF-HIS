ea6f40d8088eada2d0238788319176b7f2c72942
Arnel Marcelo <arnelmarcelo@hotmail.com>
2020-08-18 13:21:08 +0300

    Update Tables

---
+++ IspDataDb/dbo/Views/ApPaymentItems_View.sql
@@ -0,0 +1,30 @@
+﻿CREATE VIEW dbo.[ApPaymentItems_View] as
+(SELECT [IdNo]
+	  ,'CD' AS 'JournalCode'
+      ,[CadIdNo] AS 'JournalIdNo'
+      ,[ApOpenInvoiceIdNo] 
+      ,[Sequence] 
+      ,[Amount]
+      ,[DiscountTaken]
+  FROM [dbo].[CadOiItem]
+)
+union
+(SELECT [IdNo] 
+	  ,'CK'
+      ,[CkdIdNo]
+      ,[ApOpenInvoiceIdNo]
+      ,[Sequence]
+      ,[Amount]
+      ,[DiscountTaken]
+  FROM [dbo].[CkdOiItem]
+)
+UNION
+(SELECT [IdNo]
+	  ,'PC'
+      ,[PcsIdNo]
+      ,[ApOpenInvoiceIdNo]
+      ,[Sequence]
+      ,[Amount]
+      ,[DiscountTaken]
+  FROM [dbo].[PcsOiItem]
+)
---
+++ IspDataDb/dbo/Views/ApPayments_View.sql
@@ -0,0 +1,7 @@
+﻿/****** Script for SelectTopNRows command from SSMS  ******/
+CREATE VIEW ApPayments_View AS 
+SELECT [ApOpenInvoiceIdNo]
+      ,Sum([Amount]) AS 'Amount'
+      ,Sum([DiscountTaken]) AS 'DiscountTaken'
+  FROM [ISPDATA].[dbo].[ApPaymentItems_View]
+  GROUP BY apopeninvoiceidno
