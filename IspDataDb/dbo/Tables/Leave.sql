ef8ad6f695e674de17f737aa84f22e2bac093e1f
Arnel Marcelo <arnelmarcelo@hotmail.com>
2020-09-12 18:27:57 +0300

    adding leave tables

---
+++ IspDataDb/dbo/Tables/Leave.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Leave] (
+    [IdNo]                  SMALLINT      IDENTITY (1, 1) NOT NULL,
+    [AbsenceLeaveName]      VARCHAR (50)  NULL,
+    [AbsenceLeaveNameAra]   NVARCHAR (50) NULL,
+    [PaidDays]              SMALLINT      NULL,
+    [WarningDays]           SMALLINT      NULL,
+    [Cumulative]            BIT           NULL,
+    [WithMaximumCumulative] BIT           NULL,
+    [MaximumCumulativeDays] SMALLINT      NULL,
+    CONSTRAINT [PK_AbsenceLeave] PRIMARY KEY CLUSTERED ([IdNo] ASC)
+);
+
