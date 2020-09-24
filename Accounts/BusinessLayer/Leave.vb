9517688add5c3ead94205ce838110cc057911726
Arnel Marcelo <arnelmarcelo@hotmail.com>
2020-09-24 08:30:02 +0300

    editing leave entry

--- Accounts/BusinessLayer/Leave.vb
+++ Accounts/BusinessLayer/Leave.vb
@@ -20,7 +20,7 @@ Namespace BusinessLayer
         Public Property LeaveCode As String
         Public Property LeaveName As String
         Public Property LeaveNameAra As String
-        Public Property LeaveAllowed As Byte
+        Public Property LeaveAllowed As Int16
         Public Property PaidPercent As Decimal
         Public Property Cumulative As Boolean
         Public Property MaxCarryOver As Int16
