249f71abe1c381e6079ddff3b1c3226f1012f124
Arnel Marcelo <arnelmarcelo@hotmail.com>
2020-09-24 13:27:19 +0300

    UPDATED LEAVE TABLE

--- IspDataDb/dbo/Tables/Leave.sql
+++ IspDataDb/dbo/Tables/Leave.sql
@@ -1,16 +1,20 @@
 ﻿CREATE TABLE [dbo].[Leave] (
-    [IdNo]                  SMALLINT       IDENTITY (1, 1) NOT NULL,
-    [LeaveName]             VARCHAR (100)  NOT NULL,
-    [LeaveNameAra]          VARCHAR (100)  NOT NULL,
-    [NumberOfDays]          SMALLINT       NULL,
-    [Percentage]            DECIMAL (5, 2) NULL,
-    [WarningDays]           SMALLINT       NULL,
-    [Cumulative]            BIT            NULL,
-    [WithMaximumCumulative] BIT            NULL,
-    [MaximumCumulativeDays] SMALLINT       NULL,
-    [Notes]                 NVARCHAR (200) NULL,
+    [IdNo]          SMALLINT       IDENTITY (1, 1) NOT NULL,
+    [LeaveCode]     VARCHAR (3)    NULL,
+    [LeaveName]     VARCHAR (100)  NOT NULL,
+    [LeaveNameAra]  VARCHAR (100)  NOT NULL,
+    [NumberOfDays]  SMALLINT       NULL,
+    [PaidPercent]   DECIMAL (5, 2) NULL,
+    [MaxCarryOver]  SMALLINT       NULL,
+    [Cumulative]    BIT            NULL,
+    [MaxLimit]      SMALLINT       NULL,
+    [NoMaxLimit]    BIT            NULL,
+    [Notes]         NVARCHAR (200) NULL,
+    [DateTimeStamp] ROWVERSION     NULL,
     CONSTRAINT [PK_AbsenceLeave] PRIMARY KEY CLUSTERED ([IdNo] ASC)
 );
+
+
 
 
 
