949f05b4c99fa35ddc2fd3b3a7c87f61d3063dd4
Arnel Marcelo <arnelmarcelo@hotmail.com>
2020-09-24 13:27:19 +0300

    UPDATED LEAVE TABLE

--- IspDataDb/dbo/Tables/EmployeeLeave.sql
+++ IspDataDb/dbo/Tables/EmployeeLeave.sql
@@ -1,13 +1,18 @@
 ﻿CREATE TABLE [dbo].[EmployeeLeave] (
-    [IdNo]               INT      IDENTITY (1, 1) NOT NULL,
-    [EmployeeIdNo]       INT      NULL,
-    [LeaveIdNo]          SMALLINT NOT NULL,
-    [DateStart]          DATE     NULL,
-    [DateEnd]            DATE     NULL,
-    [SupervisorApproved] BIT      NULL,
-    [HRApproved]         BIT      NULL,
+    [IdNo]          INT            IDENTITY (1, 1) NOT NULL,
+    [EmployeeIdNo]  INT            NULL,
+    [LeaveIdNo]     SMALLINT       NOT NULL,
+    [LeaveStart]    DATETIME       NULL,
+    [LeaveEnd]      DATETIME       NULL,
+    [FullDayLeave]  BIT            NULL,
+    [LeaveStatus]   CHAR (1)       NULL,
+    [LeaveReason]   NVARCHAR (200) NULL,
+    [DateCreated]   NCHAR (10)     NULL,
+    [DateTimeStamp] ROWVERSION     NULL,
     CONSTRAINT [PK_EmployeeLeave] PRIMARY KEY CLUSTERED ([IdNo] ASC)
 );
+
+
 
 
 
