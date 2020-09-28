1677233f05298e8d8256f39ad684992226897afd
Arnel Marcelo <arnelmarcelo@hotmail.com>
2020-09-28 16:54:19 +0300

    added paygroup table

---
+++ IspDataDb/dbo/Tables/PayGroup.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[PayGroup] (
+    [IDNo]            SMALLINT      IDENTITY (1, 1) NOT NULL,
+    [PayGroupCode]    VARCHAR (5)   NOT NULL,
+    [PayGroupName]    VARCHAR (50)  NOT NULL,
+    [PayGroupNameAra] NVARCHAR (50) NOT NULL,
+    [ParentIdNo]      SMALLINT      NULL,
+    [Notes]           VARCHAR (255) NULL,
+    [DateTimeStamp]   ROWVERSION    NOT NULL
+);
+
---
+++ IspDataDb/dbo/Views/PayGroup_View.sql
@@ -0,0 +1,48 @@
+﻿
+
+
+
+CREATE View [dbo].[PayGroup_View] as 
+with cte as
+(
+select IDNo
+      ,PayGroupCode
+      ,PayGroupName
+      ,PayGroupNameAra
+      ,ParentIdNo
+      ,Notes
+      ,DateTimeStamp
+      ,cast(row_number()over(partition by ParentIdNo order by PayGroupName) as varchar(max)) as [path]
+      ,0 as levelnumber
+      ,row_number() over (partition by ParentIdNo order by PayGroupName) / power(10.0,0) as SortKey
+ 
+from PayGroup
+where ParentIdNo IS NULL
+union all
+select t.IDNo
+      ,t.PayGroupCode
+      ,t.PayGroupName
+      ,t.PayGroupNameAra
+      ,t.ParentIdNo
+      ,t.Notes
+      ,t.DateTimeStamp
+      ,[path] +'-'+ cast(row_number()over(partition by t.ParentIdNo order by t.PayGroupName) as varchar(max))
+      ,levelnumber+1
+      ,SortKey + row_number()over(partition by t.ParentIdNo order by t.PayGroupName) / power(10.0,levelnumber+1)
+ 
+ from
+    cte
+join PayGroup t on cte.IdNo = t.ParentIdNo
+)
+   
+select IDNo
+      ,PayGroupCode
+      ,PayGroupName
+      ,PayGroupNameAra
+      ,ParentIdNo
+      ,Notes
+      ,DateTimeStamp
+	  ,LevelNumber
+      ,[path]
+      ,SortKey
+from cte
