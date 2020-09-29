bf613f927e08643d45b3c7057259a658c7e7ef17
Arnel Marcelo <arnelmarcelo@hotmail.com>
2020-09-29 16:50:26 +0300

    update tables

--- IspDataDb/dbo/Tables/PayrollEarnAccount.sql
+++ IspDataDb/dbo/Tables/PayrollEarnAccount.sql
@@ -1,10 +1,8 @@
-﻿CREATE TABLE [dbo].[PayrollEarnAccounts] (
+﻿CREATE TABLE [dbo].[PayrollEarnAccount] (
     [IdNo]         INT      NULL,
     [EarningIdNo]  SMALLINT NULL,
     [PayGroupIdNo] SMALLINT NULL,
     [EmployeeIdNo] INT      NULL,
     [AccountIdNo]  SMALLINT NULL
 );
-
-
 
