2fa405200f26fe6cfc046515e2438125157920f4
Arnel Marcelo <arnelmarcelo@hotmail.com>
2020-10-04 15:33:25 +0300

    Editing PhoneNumbers

---
+++ IspDataDb/dbo/User Defined Types/EmployeePhoneUpdate.sql
@@ -0,0 +1,11 @@
+﻿CREATE TYPE [dbo].[EmployeePhoneUpdate] AS TABLE (
+    [AreaCode]          VARCHAR (5)  NOT NULL,
+    [EmployeeIdNo]      INT          NOT NULL,
+    [IDNo]              INT          NOT NULL,
+    [InternationalCode] VARCHAR (3)  NOT NULL,
+    [PhoneIdNo]         SMALLINT     NOT NULL,
+    [PhoneNumber]       VARCHAR (14) NOT NULL,
+    [PhoneTypeIdNo]     SMALLINT     NOT NULL,
+    [Sequence]          TINYINT      NOT NULL,
+    PRIMARY KEY CLUSTERED ([IDNo] ASC));
+
