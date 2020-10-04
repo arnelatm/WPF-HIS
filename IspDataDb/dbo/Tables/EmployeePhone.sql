893424f3a25ee9856f5730fab1d07c00cd4a0d4c
Arnel Marcelo <arnelmarcelo@hotmail.com>
2020-10-04 11:53:42 +0300

    Added EmployeePhone in Database

---
+++ IspDataDb/dbo/Tables/EmployeePhone.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[EmployeePhone] (
+    [IdNo]              INT          NULL,
+    [EmployeeIdNo]      INT          NULL,
+    [PhoneTypeIdNo]     SMALLINT     NULL,
+    [InternationalCode] VARCHAR (3)  NULL,
+    [AreaCode]          VARCHAR (5)  NULL,
+    [PhoneNumber]       VARCHAR (14) NULL
+);
+
