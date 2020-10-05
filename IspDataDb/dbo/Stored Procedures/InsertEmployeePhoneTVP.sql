8d319789a510ec6ba3e8bc02b37c121b50e50ccd
Arnel Marcelo <arnelmarcelo@hotmail.com>
2020-10-05 13:52:11 +0300

    update tables

--- IspDataDb/dbo/Stored Procedures/InsertEmployeePhoneTVP.sql
+++ IspDataDb/dbo/Stored Procedures/InsertEmployeePhoneTVP.sql
@@ -10,7 +10,7 @@
 CREATE PROC [dbo].[InsertEmployeePhoneTVP]
   @MParam EmployeePhoneInsert READONLY
 AS 
-INSERT  INTO EmployeePhone (AreaCode, EmployeeIdNo, InternationalCode, PhoneTypeIdNo, PhoneNumber, Sequence)
-        SELECT  AreaCode, EmployeeIdNo, InternationalCode, PhoneTypeIdNo, PhoneNumber, Sequence
+INSERT  INTO EmployeePhone (AreaCode, EmployeeIdNo, CountryTelCode, PhoneTypeIdNo, PhoneNumber, Sequence)
+        SELECT  AreaCode, EmployeeIdNo, CountryTelCode, PhoneTypeIdNo, PhoneNumber, Sequence
         FROM    @MParam
 SET IDENTITY_INSERT DBO.EmployeePhone ON;
--- IspDataDb/dbo/Stored Procedures/InsertPayrollDeductAccountTVP.sql
+++ IspDataDb/dbo/Stored Procedures/InsertPayrollDeductAccountTVP.sql
@@ -9,7 +9,7 @@
 CREATE PROC [dbo].[InsertPayrollDeductAccountTVP]
   @MParam PayrollDeductAccountInsert READONLY
 AS 
-INSERT  INTO PayrollDeductAccount (AccountIdNo, DeductionIdNo, PayGroupIdNo)
-        SELECT  AccountIdNo, DeductionIdNo, PayGroupIdNo
+INSERT  INTO PayrollDeductAccount (AccountIdNo, DeductionIdNo, PayGroupIdNo, Sequence)
+        SELECT  AccountIdNo, DeductionIdNo, PayGroupIdNo, Sequence
         FROM    @MParam
 SET IDENTITY_INSERT DBO.PayrollDeductAccount ON;
