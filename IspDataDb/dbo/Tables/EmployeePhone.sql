55f794881035adee4dab9a53bcc8cdbed1e4751a
Arnel Marcelo <arnelmarcelo@hotmail.com>
2020-10-06 13:00:32 +0300

    editing

--- IspDataDb/dbo/Tables/EmployeePhone.sql
+++ IspDataDb/dbo/Tables/EmployeePhone.sql
@@ -1,12 +1,15 @@
 ﻿CREATE TABLE [dbo].[EmployeePhone] (
-    [IdNo]           INT          NULL,
-    [EmployeeIdNo]   INT          NULL,
+    [IdNo]           INT          IDENTITY (1, 1) NOT NULL,
+    [EmployeeIdNo]   INT          NOT NULL,
     [PhoneTypeIdNo]  SMALLINT     NULL,
     [CountryTelIdNo] SMALLINT     NULL,
     [AreaCode]       VARCHAR (5)  NULL,
-    [PhoneNumber]    VARCHAR (14) NULL,
-    [Sequence]       TINYINT      NULL
+    [PhoneNumber]    VARCHAR (14) NOT NULL,
+    [Sequence]       TINYINT      NOT NULL,
+    CONSTRAINT [PK_EmployeePhone] PRIMARY KEY CLUSTERED ([IdNo] ASC)
 );
+
+
 
 
 
