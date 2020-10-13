b9d872d5bdaf7184b1a596896a625e12d96ced13
Arnel Marcelo <arnelmarcelo@hotmail.com>
2020-10-13 12:53:15 +0300

    Added PayCyle and payPeriod Tables

---
+++ IspDataDb/dbo/Tables/PayCycle.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[PayCycle] (
+    [IdNo]            SMALLINT      IDENTITY (1, 1) NOT NULL,
+    [PayCycleCode]    VARCHAR (5)   NOT NULL,
+    [PayCycleName]    VARCHAR (50)  NOT NULL,
+    [PayCycleNameAra] NVARCHAR (50) NOT NULL,
+    [Notes]           VARCHAR (255) NULL,
+    [DateTimeStamp]   ROWVERSION    NULL,
+    CONSTRAINT [PK__PayCycleID] PRIMARY KEY CLUSTERED ([IdNo] ASC)
+);
+
---
+++ IspDataDb/dbo/Tables/PayPeriod.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[PayPeriod] (
+    [IdNo]          SMALLINT      IDENTITY (1, 1) NOT NULL,
+    [PayCycleIdNo]  SMALLINT      NOT NULL,
+    [StartDate]     DATE          NOT NULL,
+    [EndDate]       DATE          NOT NULL,
+    [Notes]         VARCHAR (255) NULL,
+    [DateTimeStamp] ROWVERSION    NULL,
+    CONSTRAINT [PK__PayPeriodID] PRIMARY KEY CLUSTERED ([IdNo] ASC)
+);
+
