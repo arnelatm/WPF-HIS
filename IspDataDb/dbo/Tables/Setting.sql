3afa99b945ba99e5e8ea4cebbdbd38e067e45421
Arnel Marcelo <arnelmarcelo@hotmail.com>
2020-10-24 18:18:16 +0300

    Updated Setting Table

---
+++ IspDataDb/dbo/Tables/Setting.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Setting] (
+    [IdNo]        SMALLINT       NOT NULL,
+    [Group]       VARCHAR (10)   NOT NULL,
+    [SettingCode] VARCHAR (10)   NOT NULL,
+    [ValueType]   VARCHAR (2)    NULL,
+    [Value]       VARCHAR (100)  NULL,
+    [Notes]       NVARCHAR (200) NULL
+);
+
