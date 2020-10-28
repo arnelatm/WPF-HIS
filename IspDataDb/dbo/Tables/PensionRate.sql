5e10a5313415aebe27ed734f031e18ed5f2fd01d
Arnel Marcelo <arnelmarcelo@hotmail.com>
2020-10-28 12:48:27 +0300

    Update PensionScheme Entry with Pension Rates

---
+++ IspDataDb/dbo/Tables/PensionRate.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[PensionRate] (
+    [IdNo]              SMALLINT       IDENTITY (1, 1) NOT NULL,
+    [PensionSchemeIdNo] SMALLINT       NULL,
+    [LowRange]          MONEY          NULL,
+    [HighRange]         MONEY          NULL,
+    [MaxAmount]         MONEY          NULL,
+    [EmployeeShare]     DECIMAL (8, 2) NULL,
+    [EmployerShare]     DECIMAL (8, 2) NULL,
+    [DateTimeStamp]     ROWVERSION     NULL,
+    CONSTRAINT [PK_PensionRates] PRIMARY KEY CLUSTERED ([IdNo] ASC)
+);
+
