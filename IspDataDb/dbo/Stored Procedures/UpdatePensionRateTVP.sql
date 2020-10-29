c9c9c6b589c0b694d28b9eb9d434ac2959f54bdd
Arnel Marcelo <arnelmarcelo@hotmail.com>
2020-10-29 12:51:22 +0300

    updated tables

---
+++ IspDataDb/dbo/Stored Procedures/UpdatePensionRateTVP.sql
@@ -0,0 +1,32 @@
+﻿
+
+
+
+
+
+
+
+
+CREATE PROCEDURE  [dbo].[UpdatePensionRateTVP]
+  @MParam PensionRateUpdate READONLY, @GroupIdNo as INT
+AS 
+
+BEGIN
+
+-- Delete non existent records
+DELETE A
+FROM [DBO].PensionRate A WHERE A.PensionSchemeIdNo = @GroupIdNo and NOT EXISTS (SELECT * FROM @MParam where IdNo = A.IdNo )
+
+-- Update existing PensionRates
+UPDATE a 
+SET a.EmployerShare = B.EmployerShare,
+	a.EmployeeShare = B.EmployeeShare,
+	a.HighRange = B.HighRange,
+	a.LowRange = B.LowRange,
+	a.MaxAmount = B.MaxAmount,
+	a.PensionSchemeIdNo = @GroupIdNo,
+	a.[Sequence] = B.[Sequence]
+from PensionRate a INNER JOIN @MParam As b
+on a.IdNo = b.IdNo
+
+END
