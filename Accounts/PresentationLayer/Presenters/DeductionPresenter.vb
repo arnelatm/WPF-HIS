6448f0f6feb53760ec39fd3305a97c7ff2ed2747
Arnel Marcelo <arnelmarcelo@hotmail.com>
2020-10-31 17:14:12 +0300

    Editing

--- Accounts/PresentationLayer/Presenters/DeductionPresenter.vb
+++ Accounts/PresentationLayer/Presenters/DeductionPresenter.vb
@@ -1,13 +1,19 @@
-﻿Imports AATM.Accounts.PresentationLayer.Models
+﻿Imports AATM.Accounts.BusinessLayer
+Imports AATM.Accounts.PresentationLayer.Models
 Imports AATM.Accounts.PresentationLayer.Views
 Imports AATM.Accounts.PresentationLayer.Views.Interfaces
 Imports AATM.Libraries
 Imports AATM.Libraries.GlobalFuncNSub
+Imports AATM.Libraries.MessagingLibrary
 
 Namespace PresentationLayer.Presenters
 
     Public Class DeductionPresenter
         Inherits AccountsPresenter(Of IDeductionView, DeductionModel)
+
+        Protected DtInsertTable As New DataTable
+        Protected DtUpdateTable As New DataTable
+        Private ReadOnly _payrollDeductAccountModel As New ModelAccounts("PayrollDeductAccount")
 
         Public Sub New(view As IDeductionView)
             MyBase.New(view)
@@ -16,23 +22,56 @@ Namespace PresentationLayer.Presenters
             Ea = New EventAggregator()
             Ea.SubscribeEvent(Me)
 
+            DtInsertTable.Columns.Add("AccountIdNo", GetType(Int16))
+            DtInsertTable.Columns.Add("DeductionIdNo", GetType(Int16))
+            DtInsertTable.Columns.Add("PayGroupIdNo", GetType(Int16))
+            DtInsertTable.Columns.Add("Sequence", GetType(Int16))
+
+            DtUpdateTable.Columns.Add("AccountIdNo", GetType(Int16))
+            DtUpdateTable.Columns.Add("DeductionIdNo", GetType(Int16))
+            DtUpdateTable.Columns.Add("IdNo", GetType(Int32))
+            DtUpdateTable.Columns.Add("PayGroupIdNo", GetType(Int16))
+            DtUpdateTable.Columns.Add("Sequence", GetType(Int16))
+
         End Sub
 
-        'Private Sub OnBeforeSave() Handles MyBase.BeforeSave
-        '    If GetEnumCodeValue(Of DeductionTypeSelection)(View.DeductionType) = DeductionTypeSelection.Others Then
+        Public Sub OnBeforeSave() Handles MyBase.BeforeSave
+            If Not CancelSave Then
+                ViewToDataTables(View.PayrollDeductAccounts, DtInsertTable, DtUpdateTable, AddressOf FillData, AddressOf PayrollDeductAccountFilter)
+            End If
+        End Sub
 
-        '    End If
-        'End Sub
+        Private Sub FillData(ByRef item As Object, ByVal idNo As Integer, ByRef workRow As DataRow)
+            workRow("AccountIdNo") = item.AccountIdNo
+            workRow("DeductionIdNo") = View.IdNo
+            workRow("PayGroupIdNo") = item.PayGroupIdNo
+        End Sub
 
-        'Protected Overrides Function IsBizDataValid() As Boolean
-        '    Dim retValue = False
-        '    If MyBase.IsBizDataValid() Then
-        '        If View.DeductionType = EnumCode(DeductionTypeSelection.Others) Then
-        '            View.DeductionType =
-        '        End If
-        '    End If
-        '    Return retValue
-        'End Function
+        Public Function PayrollDeductAccountFilter(ByVal obj As Object) As Boolean
+            If (obj.AccountIdNo Is Nothing Or obj.AccountIdNo = 0) Then 'AndAlso (obj.PayGroupIdNo Is Nothing Or obj.PayGroupIdNo = 0) Then
+                Return False
+            End If
+            Return True
+        End Function
+
+        Public Sub SaveChildren(ByRef retVal As Integer) Handles MyBase.RecordAddedSuccessfully, MyBase.RecordUpdatedSuccessfully
+            Dim passedValue As Integer = retVal
+            retVal = UpdateChildData(_payrollDeductAccountModel, DtUpdateTable, DtInsertTable, passedValue, "DeductionIdNo")
+        End Sub
+
+        Protected Overrides Function IsBizDataValid() As Boolean
+            Dim retValue = False
+            If MyBase.IsBizDataValid() Then
+                retValue = True
+                If Not UsePayGroups() Then
+                    If View.AccountIdNo <= 0 Then
+                        Messaging.Show(True, "MsgPostingAccountMustNotBeBlank")
+                        retValue = False
+                    End If
+                End If
+            End If
+                Return retValue
+        End Function
 
     End Class
 
