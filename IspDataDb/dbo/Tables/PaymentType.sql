d265ef3f0adabd6058258213bafa1a22ac6f7c46
Arnel Marcelo <arnelmarcelo@hotmail.com>
2020-11-02 20:09:55 +0300

    updated tables

---
+++ IspDataDb/dbo/Tables/PaymentType.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[PaymentType] (
+    [IdNo]                      TINYINT        IDENTITY (1, 1) NOT NULL,
+    [PaymentTypeCode]           CHAR (1)       NULL,
+    [PaymentTypeName]           NVARCHAR (30)  NULL,
+    [PaymentTypeNameAra]        NVARCHAR (30)  NULL,
+    [AccountIdNo]               INT            NULL,
+    [Rate]                      DECIMAL (5, 2) NULL,
+    [WithBankCharges]           BIT            NULL,
+    [BankChargesAccountIdNo]    INT            NULL,
+    [BankChargesVatAccountIdNo] INT            NULL,
+    [Notes]                     NVARCHAR (255) NULL,
+    CONSTRAINT [PK_PaymentType] PRIMARY KEY CLUSTERED ([IdNo] ASC)
+);
+
--- IspDataDb/dbo/Tables/SalesCashItem.sql
+++ IspDataDb/dbo/Tables/SalesCashItem.sql
@@ -2,11 +2,13 @@
     [IdNo]             INT      IDENTITY (1, 1) NOT NULL,
     [SalesJournalIdNo] INT      NOT NULL,
     [Sequence]         SMALLINT NOT NULL,
-    [CashCode]         CHAR (1) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+    [CashCodeIdNo]     TINYINT  NOT NULL,
     [SaleAmount]       MONEY    CONSTRAINT [DF_SalesDetailItem_SaleAmount] DEFAULT ((0)) NOT NULL,
     [DepositAmount]    MONEY    CONSTRAINT [DF_SalesDetailItem_CashAmount] DEFAULT ((0)) NOT NULL,
     CONSTRAINT [PK_SalesDetailItemIdNo] PRIMARY KEY CLUSTERED ([IdNo] ASC)
 );
+
+
 
 
 
