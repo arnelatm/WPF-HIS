458b16933a898c8a1d9cc4acfe01dbcb89caf48c
Arnel Marcelo <arnelmarcelo@hotmail.com>
2020-11-03 16:47:04 +0300

    editing tables

--- IspDataDb/dbo/Stored Procedures/InsertSalesCashItemTVP.sql
+++ IspDataDb/dbo/Stored Procedures/InsertSalesCashItemTVP.sql
@@ -2,8 +2,8 @@
 CREATE PROC [dbo].[InsertSalesCashItemTVP]
   @MParam SalesCashItemInsert READONLY
 AS 
-INSERT  INTO SalesCashItem ( CashCodeIdNo, DepositAmount, SaleAmount, SalesJournalIdNo, Sequence )
-        SELECT  CashCodeIdNo, DepositAmount, SaleAmount, SalesJournalIdNo, Sequence
+INSERT  INTO SalesCashItem ( PaymentTypeIdNo, DepositAmount, SaleAmount, SalesJournalIdNo, Sequence )
+        SELECT  PaymentTypeIdNo, DepositAmount, SaleAmount, SalesJournalIdNo, Sequence
         FROM    @MParam
 SET IDENTITY_INSERT DBO.SalesCashItem ON;
 
---
+++ IspDataDb/dbo/User Defined Types/SalesCashItemUpdate3.sql
@@ -0,0 +1,9 @@
+﻿CREATE TYPE [dbo].[SalesCashItemUpdate3] AS TABLE (
+    [CashCodeIdNo]     TINYINT NOT NULL,
+    [DepositAmount]    MONEY   NULL,
+    [IDNo]             INT     NOT NULL,
+    [SaleAmount]       MONEY   NULL,
+    [SalesJournalIdNo] INT     NOT NULL,
+    [Sequence]         INT     NOT NULL,
+    PRIMARY KEY CLUSTERED ([IDNo] ASC));
+
