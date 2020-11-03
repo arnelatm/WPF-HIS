eac2cbba53f457a22f99e975a1ff67f7850fb960
Arnel Marcelo <arnelmarcelo@hotmail.com>
2020-11-03 20:02:40 +0300

    editing

---
+++ Accounts/PresentationLayer/Presenters/PaymentTypePresenter.vb
@@ -0,0 +1,20 @@
+﻿Imports AATM.Accounts.BusinessLayer
+Imports AATM.Accounts.PresentationLayer.Models
+Imports AATM.Accounts.PresentationLayer.Views.Interfaces
+Imports AATM.Libraries
+
+Namespace PresentationLayer.Presenters
+
+    Public Class PaymentTypePresenter
+        Inherits AccountsPresenter(Of IPaymentTypeView, PaymentTypeModel)
+
+        Public Sub New(view As IPaymentTypeView)
+            MyBase.New(view)
+            InitializerWithTv("PaymentType")
+            Ea = New EventAggregator()
+            Ea.SubscribeEvent(Me)
+        End Sub
+
+    End Class
+
+End Namespace
--- Accounts/PresentationLayer/Views/Interfaces/IPaymentTypeView.vb
+++ Accounts/PresentationLayer/Views/Interfaces/IPaymentTypeView.vb
@@ -5,13 +5,13 @@ Namespace PresentationLayer.Views.Interfaces
     Public Interface IPaymentTypeView
         Inherits IView
 
-        Property AccountIdNo As Int16?
-        Property BankChargesAccountIdNo As Int16?
-        Property BankChargesVatAccountIdNo As Int16?
+        Property AccountIdNo As Int16
+        Property BankChargesAccountIdNo As Int16
+        Property BankChargesVatAccountIdNo As Int16
         Property PaymentTypeCode As String
         Property PaymentTypeName As String
         Property PaymentTypeNameAra As String
-        Property IdNo As Int32
+        Property IdNo As Int16
         Property Rate As Decimal
 
     End Interface
