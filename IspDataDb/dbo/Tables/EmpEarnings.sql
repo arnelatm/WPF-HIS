6060d440768d00151efb3ce774b585c2adada8cb
Arnel Marcelo <arnelmarcelo@hotmail.com>
2020-11-08 19:48:12 +0300

    editing tables

---
+++ IspDataDb/dbo/Tables/EmpEarnings.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[EmpEarnings] (
+    [IdNo]         INT             IDENTITY (1, 1) NOT NULL,
+    [EmployeeIdNo] SMALLINT        NOT NULL,
+    [Basic]        NUMERIC (10, 2) NOT NULL,
+    [HRA]          NUMERIC (10, 2) NULL,
+    [Food]         NUMERIC (10, 2) NULL,
+    [Transport]    NUMERIC (10, 2) NULL,
+    [OTRate]       NUMERIC (10, 2) NULL,
+    [Others]       NUMERIC (10, 2) NULL,
+    [PaymentMode]  VARCHAR (20)    NULL,
+    [BankName]     VARCHAR (50)    NULL,
+    [IBAN]         VARCHAR (20)    NULL
+);
+
