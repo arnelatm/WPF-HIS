b3e58421adb60359c024134c8ad6b1bfe21b4711
Arnel Marcelo <arnelmarcelo@hotmail.com>
2020-11-19 15:42:36 +0300

    UPDATED TABLES

---
+++ IspDataDb/dbo/Stored Procedures/InsertCdOiItemTVP.sql
@@ -0,0 +1,14 @@
+﻿
+
+
+
+
+
+
+CREATE PROC [dbo].[InsertCdOiItemTVP]
+  @MParam CdOiItemInsert READONLY
+AS 
+INSERT  INTO CdOiItem ( Amount, ApOpenInvoiceIdNo, CjIdNo, DiscountTaken, Sequence )
+        SELECT  Amount, ApOpenInvoiceIdNo, CjIdNo, DiscountTaken, Sequence
+        FROM    @MParam
+SET IDENTITY_INSERT DBO.CdOiItem ON;
---
+++ IspDataDb/dbo/Views/CdJournal_View.sql
@@ -0,0 +1,13 @@
+﻿
+CREATE VIEW [dbo].[CdJournal_View]
+AS
+SELECT        dbo.CashDisbursementJournal.IdNo, dbo.CashDisbursementJournal.TransactionDate, dbo.CashDisbursementJournal.ReferenceNo, dbo.CashDisbursementJournal.Amount, 
+                         dbo.CashDisbursementJournal.AccountIdNo, dbo.CashDisbursementJournal.PaymentType, dbo.CashDisbursementJournal.PayeeIdNo, dbo.CashDisbursementJournal.PayeeName, 
+                         dbo.CashDisbursementJournal.ORNumber, dbo.CashDisbursementJournal.DiscountTaken, 
+                         dbo.CashDisbursementJournal.DiscountAccountIdNo, dbo.CashDisbursementJournal.Applied, dbo.CashDisbursementJournal.UnApplied, dbo.CashDisbursementJournal.VatNumber, 
+                         dbo.CashDisbursementJournal.VatAmount, dbo.CashDisbursementJournal.Notes, dbo.CashDisbursementJournal.Posted, dbo.CashDisbursementJournal.DateCreated, dbo.CashDisbursementJournal.Cancelled, 
+                         dbo.CashDisbursementJournal.DateTimeStamp, dbo.currency_conversion(dbo.CashDisbursementJournal.Amount) AS WordAmount, dbo.Bank.BankCode, dbo.Bank.BankNameAra, 
+                         dbo.BankAccount.BranchName
+FROM            dbo.CashDisbursementJournal INNER JOIN
+                         dbo.BankAccount ON dbo.CashDisbursementJournal.AccountIdNo = dbo.BankAccount.AccountIdNo INNER JOIN
+                         dbo.Bank ON dbo.BankAccount.BankIdNo = dbo.Bank.IdNo
