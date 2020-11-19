9101f0f199871e137bddb039a75fd84076ef8968
Arnel Marcelo <arnelmarcelo@hotmail.com>
2020-11-19 15:42:36 +0300

    UPDATED TABLES

---
+++ IspDataDb/dbo/Stored Procedures/UpdateCdOiItemTVP.sql
@@ -0,0 +1,28 @@
+﻿
+
+
+
+
+
+
+CREATE PROCEDURE  [dbo].[UpdateCdOiItemTVP]
+  @MParam CdOiItemUpdate READONLY, @GroupIdNo as INT
+AS 
+
+BEGIN
+
+-- Delete non existent records
+DELETE A
+FROM [DBO].CdOiItem A WHERE A.CdIdNo = @GroupIdNo and NOT EXISTS (SELECT * FROM @MParam where IdNo = A.IdNo )
+
+-- Update existing CdOiItems
+UPDATE a 
+SET a.Amount = B.Amount,
+	a.CjIdNo = @GroupIdNo,
+	a.DiscountTaken = B.DiscountTaken,
+	a.ApOpenInvoiceIdNo = B.ApOpenInvoiceIdNo,
+    a.[Sequence] = B.[Sequence]
+from CdOiItem a INNER JOIN @MParam As b
+on a.IDNo = b.IDNo
+
+END
---
+++ IspDataDb/dbo/User Defined Types/CdOiItemInsert.sql
@@ -0,0 +1,7 @@
+﻿CREATE TYPE [dbo].[CdOiItemInsert] AS TABLE (
+    [Amount]            MONEY NULL,
+    [ApOpenInvoiceIdNo] INT   NOT NULL,
+    [CjIdNo]            INT   NOT NULL,
+    [DiscountTaken]     MONEY NULL,
+    [Sequence]          INT   NOT NULL);
+
