ab31893fb0e379d6a1db6852f59b827236b5df4f
Arnel Marcelo <arnelmarcelo@hotmail.com>
2020-11-30 21:56:49 +0300

    edited tables

---
+++ IspDataDb/dbo/Tables/DiscountScheme.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[DiscountScheme] (
+    [IdNo]          SMALLINT       IDENTITY (1, 1) NOT NULL,
+    [Code]          VARCHAR (5)    NOT NULL,
+    [Name]          VARCHAR (50)   NOT NULL,
+    [NameAra]       NVARCHAR (50)  NULL,
+    [Note]          NVARCHAR (255) NULL,
+    [DateTimeStamp] ROWVERSION     NULL,
+    CONSTRAINT [PK_DiscountScheme] PRIMARY KEY CLUSTERED ([IdNo] ASC)
+);
+
