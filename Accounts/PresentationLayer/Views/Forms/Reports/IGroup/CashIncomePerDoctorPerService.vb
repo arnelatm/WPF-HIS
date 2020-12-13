81057189a944ebb4d131404efe5f41e3e5af6ecc
Arnel Marcelo <arnelmarcelo@hotmail.com>
2020-12-13 16:47:15 +0300

    Added Igroup Reports

---
+++ Accounts/PresentationLayer/Views/Forms/Reports/IGroup/CashIncomePerDoctorPerService.vb
@@ -0,0 +1,49 @@
+﻿Imports System.Globalization
+Imports AATM.Accounts.PresentationLayer.Presenters
+Imports AATM.Libraries.GlobalFuncNSub
+Imports AATM.Libraries.MessagingLibrary
+
+Namespace PresentationLayer.Views.Forms.Reports
+
+    Public Class CashIncomePerDoctorPerService
+
+        Public Property MainTableName As String
+        Protected SortOrderKey As String
+
+        Public Sub New()
+
+            ' This call is required by the designer.
+            InitializeComponent()
+
+            ' Add any initialization after the InitializeComponent() call.
+
+            MainTableName = "ApJournal"
+            SortOrderKey = "IdNo"
+            PresenterObj = New ReportPresenter(Me)
+            Dim currentDate = Now()
+            ' returns previous month last day
+            Dim endDate = GlobalFunctions.GregorianDateSerial(currentDate.Year, currentDate.Month, 0)
+            dtpEndingDate.Value = endDate
+            dtpBeginningDate.Value = GlobalFunctions.GregorianDateSerial(endDate.Year, endDate.Month, 1)
+        End Sub
+
+        Private Sub CButton1_ClickButtonArea(sender As Object, e As MouseEventArgs) Handles btnOk.ClickButtonArea
+            If dtpBeginningDate.Value <= dtpEndingDate.Value Then
+                Dim cBegDate As String
+                Dim cEndDate As String
+                cBegDate = Format(dtpBeginningDate.Value, "yyyy/MM/dd")
+                cEndDate = Format(dtpEndingDate.Value, "yyyy/MM/dd")
+                Dim cForm As New ReportFormIGroup("Cash Income Summary Per Month Per Service Per Doctor.Rpt", FormCulture, cBegDate, "BeginningDate", cEndDate, "EndingDate")
+                cForm.Show()
+            Else
+                Messaging.Show(True, "MsgBegDateLessThanEndDate")
+            End If
+        End Sub
+
+        Private Sub CButton2_ClickButtonArea(sender As Object, e As MouseEventArgs) Handles btnCancel.ClickButtonArea
+            Close()
+        End Sub
+
+    End Class
+
+End Namespace
---
+++ Accounts/PresentationLayer/Views/Forms/Reports/ReportFormIGroup.vb
@@ -0,0 +1,59 @@
+﻿Imports System.Configuration
+Imports System.Globalization
+Imports AATM.Accounts.PresentationLayer.Presenters
+Imports AATM.Libraries.GlobalFuncNSub
+Imports AATM.PresentationLayer.Presenters
+
+Namespace PresentationLayer.Views.Forms.Reports
+
+    Public Class ReportFormIGroup
+
+        Public Sub New(ByVal fileName As String, formCulture As CultureInfo, ByVal ParamArray args() As Object)
+
+            ' This call is required by the designer.
+            InitializeComponent()
+
+            ' Add any initialization after the InitializeComponent() call.
+            Text = fileName
+            ReportFileName = fileName
+            MainTableName = "Account"
+            GetIgroupReportProperties()
+            Dim language As String
+            language = Strings.Left(formCulture.Name, formCulture.Name.IndexOf("-", StringComparison.Ordinal))
+            PresenterObj = New ReportPresenter(Me)
+            'Dim companyName =
+            'Dim companyNameAra =
+            For i = 0 To args.Length - 1 Step 2
+                Dim value = args(i)
+                Report.SetParameterValue(args(i + 1).ToString(), ConvertObjectToType(value))
+            Next
+            Report.SetParameterValue("Language", language)
+            Report.SetParameterValue("EstablishmentName", PresenterObj.GetRecordField("Establishment", "EstablishmentName"))
+            Report.SetParameterValue("EstablishmentNameAra", PresenterObj.GetRecordField("Establishment", "EstablishmentNameAra"))
+            Report.DataSourceConnections.Clear()
+            ProcessReport()
+
+        End Sub
+
+        Public Property MainTableName As String
+
+        Private Sub GetIgroupReportProperties()
+            Dim reportPaths As String = ConfigurationManager.AppSettings.Get($"ReportPathsIGroup")
+            Dim uid As String = ConfigurationManager.AppSettings.Get("UID")
+            Dim pwd As String = ConfigurationManager.AppSettings.Get("PWD")
+            Dim server As String = ConfigurationManager.AppSettings.Get("ServerTranslator1")
+            Dim database As String = ConfigurationManager.AppSettings.Get("DATABASEIGroup")
+
+            Report.Load(reportPaths & ReportFileName)
+
+            If Report.DataSourceConnections.Count > 0 Then
+
+                Report.DataSourceConnections(0).SetConnection(server, database, uid, pwd)
+
+            End If
+
+        End Sub
+
+    End Class
+
+End Namespace
