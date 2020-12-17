52a1bec2d489875504e6d49456d23ff000498a06
Arnel Marcelo <arnelmarcelo@hotmail.com>
2020-12-17 18:50:00 +0300

    Updated Reports and Sql Functions

--- IspDataDb/dbo/Functions/FuncArStatement.sql
+++ IspDataDb/dbo/Functions/FuncArStatement.sql
@@ -18,18 +18,25 @@ RETURN
 (
 	-- Add the SELECT statement with parameter references here
 		
-	SELECT dbo.ArDetails_View.JournalCode, dbo.ArDetails_View.IdNo, dbo.ArDetails_View.Sequence, dbo.ArDetails_View.JournalIdNo, dbo.ArDetails_View.AccountIdNo, dbo.ArDetails_View.Debit, dbo.ArDetails_View.Credit, 
-           dbo.ArDetails_View.RevCostCenterIdNo, dbo.ArDetails_View.Notes, dbo.ArDetails_View.Posted, dbo.ArDetails_View.CustomerIdNo, dbo.ArDetails_View.InvoiceNo, dbo.ArDetails_View.TransactionDate, dbo.ArDetails_View.ReferenceNo, 
-           dbo.ArDetails_View.TransactionType, dbo.Account.SpecialAccount, dbo.ArDetails_View.MainNote
+	(SELECT 0 AS 'Discount', dbo.ArDetails_View.JournalCode, dbo.ArDetails_View.JournalIdNo, Sum(dbo.ArDetails_View.Debit-dbo.ArDetails_View.Credit) as 'Amount', dbo.ArDetails_View.Notes,
+           dbo.ArDetails_View.CustomerIdNo, dbo.ArDetails_View.InvoiceNo, dbo.ArDetails_View.TransactionDate, dbo.ArDetails_View.ReferenceNo, 
+           dbo.ArDetails_View.TransactionType, dbo.ArDetails_View.MainNote
 	FROM   dbo.ArDetails_View INNER JOIN dbo.Account ON dbo.ArDetails_View.AccountIdNo = dbo.Account.IDNo
-	WHERE  (dbo.Account.SpecialAccount = 'AR') and dbo.ArDetails_View.CustomerIdNo = @CustomerIdNo and dbo.ArDetails_View.TransactionDate >= @BeginningDate and dbo.ArDetails_View.TransactionDate <= @EndingDate
-
+	WHERE  (dbo.Account.SpecialAccount = 'AR' or dbo.Account.SpecialAccount='SD') and CustomerIDNO=@CustomerIdNo AND TRANSACTIONDATE>=@BeginningDate AND TRANSACTIONDATE<@EndingDate
+	Group By dbo.ArDetails_View.JournalCode, dbo.ArDetails_View.JournalIdNo,dbo.ArDetails_View.Notes, dbo.ArDetails_View.CustomerIdNo, dbo.ArDetails_View.InvoiceNo, dbo.ArDetails_View.TransactionDate, dbo.ArDetails_View.ReferenceNo, 
+             dbo.ArDetails_View.TransactionType, dbo.ArDetails_View.MainNote)
 	Union
-
-	SELECT 'BB' , 0, 1, 0, 0, 
-			iIf((Select sum(debit-credit)  from ArStatement_View where CustomerIdNo = @CustomerIdNo and transactiondate < @BeginningDate)>0,(Select sum(debit-credit)  from ArStatement_View where CustomerIdNo = @CustomerIdNo and transactiondate < @BeginningDate),0),
-			iIf((Select sum(debit-credit)  from ArStatement_View where CustomerIdNo = @CustomerIdNo and transactiondate < @BeginningDate)<0,(Select sum(debit-credit)  from ArStatement_View where CustomerIdNo = @CustomerIdNo and transactiondate < @BeginningDate),0),
-			0, 'Beginning Balance', 0, @CustomerIdNo, 'Beg.Bal.', DateAdd(day,-1,@BeginningDate), 'Beg.Bal.', 
-           'B', 'AR', 'Beginning Balance'
-	
+	(SELECT 1,dbo.ArDetails_View.JournalCode, dbo.ArDetails_View.JournalIdNo, Sum(dbo.ArDetails_View.Credit-dbo.ArDetails_View.Debit), dbo.ArDetails_View.Notes,
+           dbo.ArDetails_View.CustomerIdNo, dbo.ArDetails_View.InvoiceNo, dbo.ArDetails_View.TransactionDate, dbo.ArDetails_View.ReferenceNo, 
+           dbo.ArDetails_View.TransactionType, dbo.ArDetails_View.MainNote
+	FROM   dbo.ArDetails_View INNER JOIN dbo.Account ON dbo.ArDetails_View.AccountIdNo = dbo.Account.IDNo
+	WHERE  dbo.Account.SpecialAccount='SD' and CustomerIDNO=@CustomerIdNo  AND TRANSACTIONDATE>=@BeginningDate AND TRANSACTIONDATE<@EndingDate
+	Group By dbo.ArDetails_View.JournalCode, dbo.ArDetails_View.JournalIdNo,dbo.ArDetails_View.Notes, dbo.ArDetails_View.CustomerIdNo, dbo.ArDetails_View.InvoiceNo, dbo.ArDetails_View.TransactionDate, dbo.ArDetails_View.ReferenceNo, 
+             dbo.ArDetails_View.TransactionType, dbo.ArDetails_View.MainNote
+	)
+	Union
+	(SELECT 0,'BB',0, ISNULL((Select sum(Debit-Credit) from ArStatement_View where CustomerIdNo = @CustomerIdNo and transactiondate < @BeginningDate),0), 'Beginning Balance',
+           @CustomerIdNo , '' , DateAdd(Day,-1,@BeginningDate), '',
+           'B', 'Beginning Balance'
+	)
 )
