e5775a71f91cc5357a0570bbd2682d961a988c9c
Arnel Marcelo <arnelmarcelo@hotmail.com>
2020-12-28 19:48:59 +0300

    Updated Check Printing

---
+++ IspDataDb/dbo/Tables/Attendance.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Attendance] (
+    [IdNo]                 INT            NULL,
+    [EmployeeIdNo]         INT            NULL,
+    [PayPeriodIdNo]        SMALLINT       NULL,
+    [DaysPresent]          DECIMAL (5, 2) NULL,
+    [DaysAbsentWithPay]    DECIMAL (5, 2) NULL,
+    [DaysAbsentWithoutPay] DECIMAL (5, 2) NULL,
+    [DaysOff]              DECIMAL (5, 2) NULL
+);
+
