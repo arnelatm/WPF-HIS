f8c1cb32497c34afaa08a46567d0ad528ea4b6cf
Arnel Marcelo <arnelmarcelo@hotmail.com>
2020-12-31 13:57:11 +0300

    Edited Attendance Views and tables

---
+++ IspDataDb/dbo/User Defined Types/AttendanceItemInsert.sql
@@ -0,0 +1,9 @@
+﻿CREATE TYPE [dbo].[AttendanceItemInsert] AS TABLE (
+    [DaysAbsentWithoutPay] DECIMAL (18) NOT NULL,
+    [DaysAbsentWithPay]    DECIMAL (18) NOT NULL,
+    [DaysOff]              DECIMAL (18) NOT NULL,
+    [DaysPresent]          DECIMAL (18) NOT NULL,
+    [EmployeeIdNo]         INT          NOT NULL,
+    [PayPeriodIdNo]        SMALLINT     NOT NULL,
+    [Sequence]             INT          NOT NULL);
+
