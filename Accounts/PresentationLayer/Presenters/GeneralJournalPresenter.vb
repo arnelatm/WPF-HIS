96d1b901c3ab6b78e6eee41cbd9bb9a47ab5d1c6
Arnel Marcelo <arnelmarcelo@hotmail.com>
2021-01-02 08:08:18 +0300

    editing

--- Accounts/PresentationLayer/Presenters/GeneralJournalPresenter.vb
+++ Accounts/PresentationLayer/Presenters/GeneralJournalPresenter.vb
@@ -16,7 +16,7 @@ Namespace PresentationLayer.Presenters
 
         Protected DtInsertTable As New DataTable
         Protected DtUpdateTable As New DataTable
-        Private ReadOnly _gjJournalItemModel As New ModelAccounts("GeneralJournalItem")
+        Private ReadOnly _gjJournalItemModel As New ModelAccounts("JournalItem", Nothing, {"GeneralJournalItem_View", "UpdateGeneralJournalItemTVP", "InsertGeneralJournalItemTVP"})
         Private ReadOnly _closingEntry As Boolean
 
         Public Sub New(view As IGeneralJournalView, closingEntry As Boolean)
