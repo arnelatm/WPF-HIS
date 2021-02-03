cd11da5007b304c0073d91ccf33cf88046b0b838
Arnel Marcelo <arnelmarcelo@hotmail.com>
2021-02-03 13:03:56 +0300

    Updated Tables PCJournal

--- IspDataDb/dbo/Tables/PcJournal.sql
+++ IspDataDb/dbo/Tables/PcJournal.sql
@@ -5,6 +5,7 @@
     [Amount]              MONEY          NOT NULL,
     [AccountIdNo]         SMALLINT       NOT NULL,
     [PaymentType]         CHAR (1)       COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+    [PayType]             CHAR (1)       NULL,
     [PayeeIdNo]           INT            NULL,
     [PayeeName]           NVARCHAR (50)  COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
     [CheckNumber]         VARCHAR (10)   COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
@@ -25,6 +26,8 @@
     [DateTimeStamp]       ROWVERSION     NULL,
     CONSTRAINT [PK_PcJournal1] PRIMARY KEY CLUSTERED ([IdNo] ASC)
 );
+
+
 
 
 
