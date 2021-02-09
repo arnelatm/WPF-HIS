d9bc79dfe2a1e043f7dfc51b574ff59e6fc76c3a
Arnel Marcelo <arnelmarcelo@hotmail.com>
2021-02-09 16:42:52 +0300

    Editing

---
+++ ChartBalance.sql
@@ -0,0 +1,254 @@
+﻿USE [ISPDATA]
+GO
+SET IDENTITY_INSERT [dbo].[ChartBalance] ON 
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (1, 2017, 106, 487308.7600, 0.0000)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (2, 2017, 112, 584565.6000, 0.0000)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (3, 2017, 114, 172017.7500, 0.0000)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (4, 2017, 118, 300269.8400, 0.0000)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (5, 2017, 119, 119409.3600, 0.0000)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (6, 2017, 138, 1340778.0000, 0.0000)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (7, 2017, 140, 541922.0000, 0.0000)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (8, 2017, 142, 435065.0000, 0.0000)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (9, 2017, 144, 2017445.0000, 0.0000)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (10, 2017, 146, 352341.0000, 0.0000)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (11, 2017, 148, 57100.0000, 0.0000)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (12, 2017, 150, 5796.0000, 0.0000)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (13, 2017, 168, 0.0000, 304432.0000)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (14, 2017, 170, 0.0000, 381774.8300)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (15, 2017, 172, 0.0000, 532748.0000)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (16, 2017, 174, 0.0000, 1621137.6800)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (17, 2017, 176, 0.0000, 221470.4200)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (18, 2017, 178, 0.0000, 33691.3800)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (19, 2017, 180, 0.0000, 5189.3300)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (20, 2017, 193, 0.0000, 1517951.8700)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (21, 2017, 210, 0.0000, 12258.9000)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (22, 2017, 310, 0.0000, 300000.0000)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (23, 2017, 320, 0.0000, 584787.3800)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (24, 2017, 212, 0.0000, 5000.0000)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (25, 2017, 213, 0.0000, 475473.0000)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (26, 2017, 208, 0.0000, 146793.0000)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (27, 2017, 136, 351944.0000, 0.0000)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (28, 2017, 166, 0.0000, 31490.0300)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (29, 2017, 152, 165893.0000, 0.0000)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (30, 2017, 181, 0.0000, 87203.7000)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (31, 2017, 143, 242870.0000, 0.0000)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (32, 2017, 202, 0.0000, 352485.7500)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (33, 2017, 203, 0.0000, 152657.4100)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (34, 2017, 204, 0.0000, 215575.4500)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (35, 2017, 205, 0.0000, 221155.8000)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (36, 2017, 125, 999.4200, 0.0000)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (37, 2017, 223, 0.0000, 5063.8000)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (38, 2017, 207, 32615.0000, 0.0000)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (39, 2018, 106, 479360.8200, 0.0000)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (40, 2018, 107, 0.0400, 0.0000)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (41, 2018, 111, 1224.7300, 0.0000)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (42, 2018, 112, 552833.2700, 0.0000)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (43, 2018, 114, 144393.8000, 0.0000)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (44, 2018, 118, 341037.6800, 0.0000)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (45, 2018, 119, 96081.0200, 0.0000)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (46, 2018, 125, 999.4200, 0.0000)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (47, 2018, 127, 5000.0000, 0.0000)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (48, 2018, 136, 511944.0000, 0.0000)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (49, 2018, 138, 1663410.0000, 0.0000)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (50, 2018, 140, 606669.0000, 0.0000)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (51, 2018, 142, 451548.0000, 0.0000)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (52, 2018, 143, 281570.0000, 0.0000)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (53, 2018, 144, 2175195.0000, 0.0000)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (54, 2018, 146, 352341.0000, 0.0000)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (55, 2018, 148, 57100.0000, 0.0000)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (56, 2018, 150, 7731.0000, 0.0000)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (57, 2018, 152, 183128.0000, 0.0000)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (58, 2018, 166, 0.0000, 48533.0300)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (59, 2018, 168, 0.0000, 360906.0000)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (60, 2018, 170, 0.0000, 401292.1600)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (61, 2018, 172, 0.0000, 586138.6000)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (62, 2018, 174, 0.0000, 1711750.6800)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (63, 2018, 176, 0.0000, 266345.4200)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (64, 2018, 178, 0.0000, 37538.8800)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (65, 2018, 180, 0.0000, 5292.0800)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (66, 2018, 181, 0.0000, 104293.4900)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (67, 2018, 193, 0.0000, 1790351.6700)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (68, 2018, 202, 0.0000, 390119.9100)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (69, 2018, 203, 0.0000, 103175.9600)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (70, 2018, 204, 0.0000, 148923.2500)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (71, 2018, 205, 0.0000, 241386.8000)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (72, 2018, 207, 0.0000, 54460.0000)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (73, 2018, 208, 0.0000, 146793.0000)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (74, 2018, 210, 0.0000, 53633.3800)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (75, 2018, 213, 0.0000, 385553.0000)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (76, 2018, 223, 0.0000, 4972.0000)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (77, 2018, 226, 0.0000, 10230.0000)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (78, 2018, 310, 0.0000, 300000.0000)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (79, 2018, 320, 0.0000, 759877.4700)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (80, 2019, 104, 16894.6700, 0.0000)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (81, 2019, 106, 311831.9800, 0.0000)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (82, 2019, 107, 20637.1400, 0.0000)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (83, 2019, 111, 2893.5300, 0.0000)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (84, 2019, 127, 5000.0000, 0.0000)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (85, 2019, 112, 526218.6700, 0.0000)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (86, 2019, 114, 160915.9800, 0.0000)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (87, 2019, 118, 184209.0600, 0.0000)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (88, 2019, 119, 50879.1200, 0.0000)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (89, 2019, 136, 511944.0000, 0.0000)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (90, 2019, 138, 1663410.0000, 0.0000)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (91, 2019, 140, 694799.0000, 0.0000)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (92, 2019, 142, 733118.0000, 0.0000)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (93, 2019, 144, 2160695.0000, 0.0000)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (94, 2019, 146, 352341.0000, 0.0000)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (95, 2019, 148, 57100.0000, 0.0000)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (96, 2019, 150, 7731.0000, 0.0000)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (97, 2019, 152, 202346.5700, 0.0000)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (98, 2019, 166, 0.0000, 65701.0300)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (99, 2019, 168, 0.0000, 417381.0000)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (100, 2019, 170, 0.0000, 432346.1100)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (101, 2019, 172, 0.0000, 640653.8500)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (102, 2019, 174, 0.0000, 1761545.1700)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (103, 2019, 176, 0.0000, 311220.4200)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (104, 2019, 178, 0.0000, 41386.3800)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (105, 2019, 180, 0.0000, 5423.8300)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (106, 2019, 181, 0.0000, 122442.9100)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (107, 2019, 202, 0.0000, 297895.6500)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (108, 2019, 203, 0.0000, 34041.5100)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (109, 2019, 204, 0.0000, 180066.4600)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (110, 2019, 205, 0.0000, 240078.1900)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (111, 2019, 207, 0.0000, 72502.0000)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (112, 2019, 210, 0.0000, 63687.7600)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (113, 2019, 226, 0.0000, 11651.4400)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (114, 2019, 228, 0.0000, 0.0000)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (115, 2019, 229, 0.0000, 0.0000)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (116, 2019, 231, 0.0000, 0.0000)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (117, 2019, 232, 0.0000, 0.0000)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (118, 2019, 233, 0.0000, 0.0000)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (119, 2019, 242, 0.0000, 32634.5300)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (120, 2019, 213, 0.0000, 320029.0000)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (121, 2019, 208, 0.0000, 146793.0000)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (122, 2019, 310, 0.0000, 300000.0000)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (123, 2019, 320, 0.0000, 297971.2300)
+GO
+INSERT [dbo].[ChartBalance] ([IdNo], [Year], [AccountIdNo], [Debit], [Credit]) VALUES (124, 2019, 193, 0.0000, 1867513.2500)
+GO
+SET IDENTITY_INSERT [dbo].[ChartBalance] OFF
+GO
