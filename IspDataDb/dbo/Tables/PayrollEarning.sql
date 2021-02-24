a805781e9ade5c9e70596f68f7c3a199c547b30a
Arnel Marcelo <arnelmarcelo@hotmail.com>
2021-02-24 13:01:53 +0300

    Editing

--- IspDataDb/dbo/Tables/PayrollEarning.sql
+++ IspDataDb/dbo/Tables/PayrollEarning.sql
@@ -1,10 +1,12 @@
 ﻿CREATE TABLE [dbo].[PayrollEarning] (
-    [IdNo]          INT   NULL,
-    [PayPeriodIdNo] INT   NULL,
-    [EmployeeIdNo]  INT   NULL,
-    [EarningIdNo]   INT   NULL,
-    [Amount]        MONEY NULL
+    [IdNo]         INT      NULL,
+    [PayrollIdNo]  SMALLINT NULL,
+    [EmployeeIdNo] INT      NULL,
+    [EarningIdNo]  INT      NULL,
+    [Amount]       MONEY    NULL
 );
+
+
 
 
 
