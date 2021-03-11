a3354ebca5328b7393e3bb695580fae7e6feb1c6
Arnel Marcelo <arnelmarcelo@hotmail.com>
2021-03-11 10:57:16 +0300

    Editing

--- Accounts/DataLayer/AdoNet/PayrollEarningDao.vb
+++ Accounts/DataLayer/AdoNet/PayrollEarningDao.vb
@@ -10,35 +10,52 @@ Namespace DataLayer.AdoNet
 
     Public Class PayrollEarningDao
         Inherits AccountsDao
-        Implements IDaoChild(Of PayrollEarning), IDaoTvp(Of PayrollEarning)
+        Implements IDaoChild(Of PayrollEarning), IDaoTvp(Of PayrollEarning), IDaoGetRecords(Of PayrollEarning), IDaoGetRecord(Of PayrollEarning)
 
-        Private ReadOnly Db As New Db()
+        Private ReadOnly _db As New Db()
+
+        Private Const FieldList = "Amount," &
+                                  "EarningIdNo," &
+                                  "EmployeeIdNo," &
+                                  "IdNo," &
+                                  "PayrollIdNo"
 
         Public Function GetRecordsWithGroupIdNo(idNo, Optional sortExpression = Nothing) As List(Of PayrollEarning) Implements IDaoChild(Of PayrollEarning).GetRecordsWithGroupIdNo
             Dim sql As String =
-                    " SELECT " &
-                    "Amount," &
-                    "EarningIdNo," &
-                    "EmployeeIdNo," &
-                    "IdNo," &
-                    "PayrollIdNo" &
+                    " SELECT " & FieldList &
                     " FROM [PayrollEarning]" &
                     " WHERE PayrollIdNo = @IdNo " &
                     " ORDER BY EmployeeIdNo,EarningIdNo"
             Dim params() As Object = {"@IdNo", idNo}
-            Return Db.Read(sql, Make, params).ToList()
+            Return _db.Read(sql, Make, params).ToList()
         End Function
 
         Public Function DelUpdateTvp(ByRef tvpTable As DataTable, groupIdNo As Integer) As Integer Implements IDaoChild(Of PayrollEarning).DelUpdateTvp
-            Return Db.DelUpdateTvp("UpdatePayrollEarningTVP", tvpTable, "@MParam", groupIdNo)
+            Return _db.DelUpdateTvp("UpdatePayrollEarningTVP", tvpTable, "@MParam", groupIdNo)
         End Function
 
         Public Function InsertTvp(ByRef tvpTable As DataTable) As Integer Implements IDaoChild(Of PayrollEarning).InsertTvp
-            Return Db.InsertTvp("InsertPayrollEarningTVP", tvpTable)
+            Return _db.InsertTvp("InsertPayrollEarningTVP", tvpTable)
         End Function
 
         Public Function UpdateInsertTvp(ByRef updateTvpTable As DataTable, ByRef insertTvpTable As DataTable, ByVal groupIdNo As Integer) As Integer Implements IDaoTvp(Of PayrollEarning).UpdateInsertTvp
-            Return Db.UpdateInsertTvp("UpdateInsertPayrollEarningTVP", updateTvpTable, insertTvpTable, groupIdNo)
+            Return _db.UpdateInsertTvp("UpdateInsertPayrollEarningTVP", updateTvpTable, insertTvpTable, groupIdNo)
+        End Function
+
+        Public Function GetRecords(Optional filter As String = Nothing) As List(Of PayrollEarning) Implements IDaoGetRecords(Of PayrollEarning).GetRecords
+            Dim sql As String = "SELECT " &
+                                FieldList &
+                                " FROM [PayrollEarning]" &
+                                IIf(filter Is Nothing, "", " WHERE " & filter)
+            Return _db.Read(sql, Make).ToList()
+        End Function
+
+        Public Function GetRecord(Optional filter As String = Nothing) As PayrollEarning Implements IDaoGetRecord(Of PayrollEarning).GetRecord
+            Dim sql As String = "SELECT Top 1 " &
+                                FieldList &
+                                " FROM [PayrollEarning]" &
+                                IIf(filter Is Nothing, "", " WHERE " & filter)
+            Return _db.Read(sql, Make).FirstOrDefault()
         End Function
 
         Private Shared ReadOnly Make As Func(Of IDataReader, PayrollEarning) =
