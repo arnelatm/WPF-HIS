b59e40fab9cf58243bf9a6381a5098955aa8e0cd
Arnel Marcelo <arnelmarcelo@hotmail.com>
2021-03-18 15:35:53 +0300

    Updated Tables added PayELement for Earning/Deduction

---
+++ IspDataDb/dbo/User Defined Types/PayElementItemInsert_1.sql
@@ -0,0 +1,7 @@
+﻿CREATE TYPE [dbo].[PayElementItemInsert] AS TABLE (
+    [ParentIdNo]     SMALLINT        NOT NULL,
+    [PayElementIdNo] SMALLINT        NOT NULL,
+    [FactorType]     CHAR (1)        NOT NULL,
+    [FactorValue]    DECIMAL (10, 4) NOT NULL,
+    [Sequence]       SMALLINT        NOT NULL);
+
---
+++ IspDataDb/dbo/Views/EmployeePayElement_View_1.sql
@@ -0,0 +1,10 @@
+﻿
+
+CREATE VIEW [dbo].[EmployeePayElement_View]
+AS
+SELECT        dbo.EmployeePayElement.IdNo, dbo.EmployeePayElement.EmployeeIdNo, dbo.EmployeePayElement.PayElementIdNo, dbo.EmployeePayElement.Amount, dbo.PayElement.PayElementCode, dbo.PayElement.PayElementName, dbo.PayElement.PayElementNameAra, 
+                         dbo.PayElement.Frequency, dbo.PayElement.PayElementType, dbo.EmployeePayElement.Sequence, dbo.PayElement.CalculationType, dbo.PayElement.DefaultQuantity, dbo.PayElement.FactorValue, dbo.PayElement.FactorType, 
+                         dbo.PayElement.BasePaymentIdNo, dbo.PayElement.IncludeInEos, dbo.PayElement.Taxable, dbo.PayElement.Unit, dbo.PayElement.UsePayGroups, dbo.PayElement.AccountIdNo, 
+                         dbo.EmployeePayElement.Rate AS 'Rate'
+FROM            dbo.EmployeePayElement INNER JOIN
+                         dbo.PayElement ON dbo.EmployeePayElement.PayElementIdNo = dbo.PayElement.IdNo
