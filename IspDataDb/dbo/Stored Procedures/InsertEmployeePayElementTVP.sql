c8d7a50d6ca721fe8fbb8ed69562ed9ccb27da14
Arnel Marcelo <arnelmarcelo@hotmail.com>
2021-03-25 13:56:49 +0300

    Updated EMployee Table Sql

--- IspDataDb/dbo/Stored Procedures/InsertEmployeePayElementTVP.sql
+++ IspDataDb/dbo/Stored Procedures/InsertEmployeePayElementTVP.sql
@@ -4,10 +4,11 @@
 
 
 
+
 CREATE PROC [dbo].[InsertEmployeePayElementTVP]
   @MParam EmployeePayElementInsert READONLY
 AS 
-INSERT  INTO EmployeePayElement ( Amount, PayElementIdNo, EmployeeIdNo, Rate, Sequence )
-        SELECT  Amount, PayElementIdNo, EmployeeIdNo, Rate, Sequence
+INSERT  INTO EmployeePayElement ( Amount, PayElementIdNo, EmployeeIdNo, Rate, Sequence, Unit )
+        SELECT  Amount, PayElementIdNo, EmployeeIdNo, Rate, Sequence, Unit
         FROM    @MParam
 SET IDENTITY_INSERT DBO.EmployeePayElement ON;
