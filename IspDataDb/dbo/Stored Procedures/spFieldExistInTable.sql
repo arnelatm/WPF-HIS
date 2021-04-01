a27f77022a8d00ed9ce7eff89f79b3b0c89fc900
Arnel Marcelo <arnelmarcelo@hotmail.com>
2021-04-01 13:29:23 +0300

    Added stored procedure to check for existense of a field in a table

---
+++ IspDataDb/dbo/Stored Procedures/spFieldExistInTable.sql
@@ -0,0 +1,16 @@
+﻿CREATE Procedure spFieldExistInTable
+@tableName as nVarChar(50),
+@fieldName as nVarChar(50)
+as
+begin
+	Declare @retValue as int
+	IF COL_LENGTH(@tableName,@fieldName) IS NOT NULL
+		set @retValue = 1
+	ELSE
+		set @retValue = 0
+	return @retValue
+end
+
+declare @x as nVarChar(20)
+Execute @x = spFieldExistInTable @TableName = 'Employee', @fieldName = 'EmployeeName'
+print @x
