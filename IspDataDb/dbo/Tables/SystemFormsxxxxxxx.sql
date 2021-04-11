d8eb892a3eef5a3dc3b377fbe885fc2b029c3966
Arnel Marcelo <arnelmarcelo@hotmail.com>
2021-04-11 16:52:22 +0300

    Statement of EMployee loans (IGroup)

--- IspDataDb/dbo/Tables/SystemFormsxxxxxxx.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[SystemFormsxxxxxxx] (
-    [IdNo]                SMALLINT     IDENTITY (1, 1) NOT NULL,
-    [FormName]            VARCHAR (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-    [DefaultValuesEnable] BIT          NULL,
-    CONSTRAINT [PK_SystemForms] PRIMARY KEY CLUSTERED ([IdNo] ASC)
-);
-
