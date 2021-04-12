5914d44e2dd38195ba7f9d8377e1288e16dbea6d
Arnel Marcelo <arnelmarcelo@hotmail.com>
2021-04-12 12:05:27 +0300

    Updated Tables/Views

--- IspDataDb/dbo/Tables/SecurityGroup.sql
+++ IspDataDb/dbo/Tables/SecurityGroup.sql
@@ -1,5 +1,5 @@
 ﻿CREATE TABLE [dbo].[SecurityGroup] (
-    [IdNo]                 SMALLINT      IDENTITY (1, 1) NOT NULL,
+    [IdNo]                 INT           IDENTITY (1, 1) NOT NULL,
     [SecurityGroupName]    VARCHAR (50)  NULL,
     [ParentIdNo]           SMALLINT      NULL,
     [Notes]                VARCHAR (100) NULL,
@@ -9,6 +9,8 @@
     CONSTRAINT [PK_IDNo] PRIMARY KEY CLUSTERED ([IdNo] ASC),
     CONSTRAINT [IX_SecurityGroupName] UNIQUE NONCLUSTERED ([SecurityGroupName] ASC)
 );
+
+
 
 
 
---
+++ IspDataDb/dbo/Views/SecurityObject_View1.sql
@@ -0,0 +1,24 @@
+﻿
+Create View [dbo].[SecurityObject_View1]
+as
+with parentChildResult as
+( select IdNo,
+		 SecurityObjectName,
+		 ParentIdNo,
+	 	 cast('' as nvarchar(max)) as ParentName
+	from SecurityObject
+	where ParentIdNo is null
+  union all
+	select i2.IdNo,
+	i2.SecurityObjectName,
+	i2.ParentIdNo,
+	parentChildResult.ParentName + ' > ' + parentChildResult.SecurityObjectName
+	from SecurityObject as i2
+	inner join parentChildResult
+	on parentChildResult.IdNo = i2.ParentIdNo
+)     
+select a.IdNo,SecurityObjectCode,a.SecurityObjectName,SecurityObjectNameAra,a.ParentIdNo,a.SystemViewIdNo,a.ManuallyAdded,a.Notes,
+	IIf(ParentName = '',b.SecurityObjectName,stuff(ParentName, 1, 3, '') + ' > ' + b.SecurityObjectName) as FullPathName
+	from SecurityObject a
+	inner Join parentChildResult b
+	on a.IdNo = b.IdNo
