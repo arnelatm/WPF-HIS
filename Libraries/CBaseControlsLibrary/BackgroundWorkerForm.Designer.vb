2f9952364baffe819d5719aff95b64cd37260bb2
Arnel Marcelo <arnelmarcelo@hotmail.com>
2021-05-05 13:37:11 +0300

    Editing progressbar form

---
+++ Libraries/CBaseControlsLibrary/BackgroundWorkerForm.Designer.vb
@@ -0,0 +1,100 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class BackgroundWorkerForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.cancelWorkButton = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lblPercent = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.SuspendLayout
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(3, 12)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(279, 23)
+        Me.ProgressBar1.TabIndex = 0
+        '
+        'cancelWorkButton
+        '
+        Me.cancelWorkButton.Location = New System.Drawing.Point(207, 41)
+        Me.cancelWorkButton.Name = "cancelWorkButton"
+        Me.cancelWorkButton.Size = New System.Drawing.Size(75, 23)
+        Me.cancelWorkButton.TabIndex = 1
+        Me.cancelWorkButton.Text = "Cancel"
+        Me.cancelWorkButton.UseVisualStyleBackColor = true
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = true
+        Me.Label1.Location = New System.Drawing.Point(3, 42)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(57, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Progress : "
+        '
+        'lblPercent
+        '
+        Me.lblPercent.AutoSize = true
+        Me.lblPercent.Location = New System.Drawing.Point(56, 42)
+        Me.lblPercent.Name = "lblPercent"
+        Me.lblPercent.Size = New System.Drawing.Size(39, 13)
+        Me.lblPercent.TabIndex = 3
+        Me.lblPercent.Text = "Label2"
+        Me.lblPercent.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = true
+        Me.Label3.Location = New System.Drawing.Point(92, 42)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(15, 13)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "%"
+        '
+        'BackgroundWorkerForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6!, 13!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 67)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.lblPercent)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.cancelWorkButton)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Name = "BackgroundWorkerForm"
+        Me.Text = "BackgroundWorkerForm"
+        Me.ResumeLayout(false)
+        Me.PerformLayout
+
+End Sub
+
+    Friend WithEvents BackgroundWorker1 As ComponentModel.BackgroundWorker
+    Friend WithEvents ProgressBar1 As Windows.Forms.ProgressBar
+    Friend WithEvents cancelWorkButton As Windows.Forms.Button
+    Friend WithEvents Label1 As Windows.Forms.Label
+    Friend WithEvents lblPercent As Windows.Forms.Label
+    Friend WithEvents Label3 As Windows.Forms.Label
+End Class
---
+++ Libraries/CBaseControlsLibrary/BackgroundWorkerForm.vb
@@ -0,0 +1,207 @@
+﻿Imports System.ComponentModel
+Imports System.Drawing
+Imports System.Windows.Forms
+
+Public Class BackgroundWorkerForm
+
+#Region "Fields"
+
+    Private ReadOnly _onDoWork As DoWorkEventHandler
+    Private ReadOnly _onProgressChanged As ProgressChangedEventHandler
+    Private ReadOnly _onRunWorkerCompleted As RunWorkerCompletedEventHandler
+
+#End Region 'Fields
+
+#Region "Constructors"
+
+    ''' <summary>
+    ''' Creates a new instance of the <see cref="BackgroundWorkerForm"/> class.
+    ''' </summary>
+    ''' <remarks>
+    ''' Parameter less constructor is private to ensure handlers are provided for <see cref="BackgroundWorker"/>.
+    ''' </remarks>
+    Private Sub New()
+        ' This call is required by the designer.
+        InitializeComponent()
+    End Sub
+
+    ''' <summary>
+    ''' Creates a new instance of the <see cref="BackgroundWorkerForm" /> class.
+    ''' </summary>
+    ''' <param name="onDoWork">
+    ''' Handler for the <see cref="BackgroundWorker.DoWork">RunWorkerCompleted</see> event of a <see cref="BackgroundWorker"/>.
+    ''' </param>
+    Public Sub New(onDoWork As DoWorkEventHandler)
+        Me.New()
+
+        _onDoWork = onDoWork
+
+        'AddHandler is used for local event handlers so that remote event handlers can be registered first and thus executed first.
+
+        'Remote event handlers
+        AddHandler BackgroundWorker1.DoWork, onDoWork
+
+        'Local event handlers
+        AddHandler BackgroundWorker1.RunWorkerCompleted, AddressOf BackgroundWorker1_RunWorkerCompleted
+    End Sub
+
+    ''' <summary>
+    ''' Creates a new instance of the <see cref="BackgroundWorkerForm" /> class.
+    ''' </summary>
+    ''' <param name="onDoWork">
+    ''' Handler for the <see cref="BackgroundWorker.DoWork">RunWorkerCompleted</see> event of a <see cref="BackgroundWorker"/>.
+    ''' </param>
+    ''' <param name="onProgressChanged">
+    ''' Handler for the <see cref="BackgroundWorker.ProgressChanged">RunWorkerCompleted</see> event of a <see cref="BackgroundWorker"/>.
+    ''' </param>
+    Public Sub New(onDoWork As DoWorkEventHandler,
+                   onProgressChanged As ProgressChangedEventHandler)
+        Me.New()
+
+        _onDoWork = onDoWork
+        _onProgressChanged = onProgressChanged
+
+        'AddHandler is used for local event handlers so that remote event handlers can be registered first and thus executed first.
+
+        'Remote event handlers
+        AddHandler BackgroundWorker1.DoWork, onDoWork
+        AddHandler BackgroundWorker1.ProgressChanged, onProgressChanged
+
+        'Local event handlers
+        AddHandler BackgroundWorker1.ProgressChanged, AddressOf BackgroundWorker1_ProgressChanged
+        AddHandler BackgroundWorker1.RunWorkerCompleted, AddressOf BackgroundWorker1_RunWorkerCompleted
+
+        'A ProgressChanged handler has been provided so the ProgressBar will be updated explicitly based on the BackgroundWorker.
+        BackgroundWorker1.WorkerReportsProgress = True
+        ProgressBar1.Style = ProgressBarStyle.Continuous
+    End Sub
+
+    ''' <summary>
+    ''' Creates a new instance of the <see cref="BackgroundWorkerForm" /> class.
+    ''' </summary>
+    ''' <param name="onDoWork">
+    ''' Handler for the <see cref="BackgroundWorker.DoWork">RunWorkerCompleted</see> event of a <see cref="BackgroundWorker"/>.
+    ''' </param>
+    ''' <param name="onRunWorkerCompleted">
+    ''' Handler for the <see cref="BackgroundWorker.RunWorkerCompleted">RunWorkerCompleted</see> event of a <see cref="BackgroundWorker"/>.
+    ''' </param>
+    Public Sub New(onDoWork As DoWorkEventHandler,
+                   onRunWorkerCompleted As RunWorkerCompletedEventHandler)
+        Me.New()
+
+        _onDoWork = onDoWork
+        _onRunWorkerCompleted = onRunWorkerCompleted
+
+        'AddHandler is used for local event handlers so that remote event handlers can be registered first and thus executed first.
+
+        'Remote event handlers
+        AddHandler BackgroundWorker1.DoWork, onDoWork
+        AddHandler BackgroundWorker1.RunWorkerCompleted, onRunWorkerCompleted
+
+        'Local event handlers
+        AddHandler BackgroundWorker1.RunWorkerCompleted, AddressOf BackgroundWorker1_RunWorkerCompleted
+    End Sub
+
+    ''' <summary>
+    ''' Creates a new instance of the <see cref="BackgroundWorkerForm" /> class.
+    ''' </summary>
+    ''' <param name="onDoWork">
+    ''' Handler for the <see cref="BackgroundWorker.DoWork">RunWorkerCompleted</see> event of a <see cref="BackgroundWorker"/>.
+    ''' </param>
+    ''' <param name="onProgressChanged">
+    ''' Handler for the <see cref="BackgroundWorker.ProgressChanged">RunWorkerCompleted</see> event of a <see cref="BackgroundWorker"/>.
+    ''' </param>
+    ''' <param name="onRunWorkerCompleted">
+    ''' Handler for the <see cref="BackgroundWorker.RunWorkerCompleted">RunWorkerCompleted</see> event of a <see cref="BackgroundWorker"/>.
+    ''' </param>
+    Public Sub New(onDoWork As DoWorkEventHandler,
+                   onProgressChanged As ProgressChangedEventHandler,
+                   onRunWorkerCompleted As RunWorkerCompletedEventHandler)
+        Me.New()
+
+        _onDoWork = onDoWork
+        _onProgressChanged = onProgressChanged
+        _onRunWorkerCompleted = onRunWorkerCompleted
+
+        'AddHandler is used for local event handlers so that remote event handlers can be registered first and thus executed first.
+
+        'Remote event handlers
+        AddHandler BackgroundWorker1.DoWork, onDoWork
+        AddHandler BackgroundWorker1.ProgressChanged, onProgressChanged
+        AddHandler BackgroundWorker1.RunWorkerCompleted, onRunWorkerCompleted
+
+        'Local event handlers
+        AddHandler BackgroundWorker1.ProgressChanged, AddressOf BackgroundWorker1_ProgressChanged
+        AddHandler BackgroundWorker1.RunWorkerCompleted, AddressOf BackgroundWorker1_RunWorkerCompleted
+
+        'A ProgressChanged handler has been provided so the ProgressBar will be updated explicitly based on the BackgroundWorker.
+        BackgroundWorker1.WorkerReportsProgress = True
+        ProgressBar1.Style = ProgressBarStyle.Continuous
+    End Sub
+
+#End Region 'Constructors
+
+#Region "Properties"
+
+    Public WriteOnly Property SupportsCancellation As Boolean
+        Set
+            BackgroundWorker1.WorkerSupportsCancellation = Value
+
+            'If the worker can be cancelled, show the Cancel button and make the form big enough to see it.
+            cancelWorkButton.Visible = Value
+            ClientSize = New Size(284,
+                                  If(Value, 76, 47))
+        End Set
+    End Property
+
+#End Region 'Properties
+
+#Region "Methods"
+
+    Private Sub BackgroundWorkerForm_Shown(sender As Object, e As EventArgs) Handles Me.Shown
+        'Start the background work when the form is displayed.
+        BackgroundWorker1.RunWorkerAsync()
+    End Sub
+
+    Private Sub cancelWorkButton_Click(sender As Object, e As EventArgs) Handles cancelWorkButton.Click
+        'Disable the button to prevent another click.
+        cancelWorkButton.Enabled = False
+
+        'Cancel the background work.
+        BackgroundWorker1.CancelAsync()
+    End Sub
+
+    Private Sub BackgroundWorker1_ProgressChanged(sender As Object, e As ProgressChangedEventArgs)
+        'Update the ProgressBar.
+        ProgressBar1.Value = e.ProgressPercentage
+        lblPercent.Text = e.ProgressPercentage.ToString()
+    End Sub
+
+    Private Sub BackgroundWorker1_RunWorkerCompleted(sender As Object, e As RunWorkerCompletedEventArgs)
+        'Close the form when the work is done.
+        Close()
+    End Sub
+
+    Private Sub BackgroundWorkerForm_FormClosing(sender As Object, e As FormClosingEventArgs) Handles Me.FormClosing
+        'Remote event handlers.
+
+        If _onDoWork IsNot Nothing Then
+            RemoveHandler BackgroundWorker1.DoWork, _onDoWork
+        End If
+
+        If _onProgressChanged IsNot Nothing Then
+            RemoveHandler BackgroundWorker1.ProgressChanged, _onProgressChanged
+        End If
+
+        If _onRunWorkerCompleted IsNot Nothing Then
+            RemoveHandler BackgroundWorker1.RunWorkerCompleted, _onRunWorkerCompleted
+        End If
+
+        'Local event handlers
+        RemoveHandler BackgroundWorker1.ProgressChanged, AddressOf BackgroundWorker1_ProgressChanged
+        RemoveHandler BackgroundWorker1.RunWorkerCompleted, AddressOf BackgroundWorker1_RunWorkerCompleted
+    End Sub
+
+#End Region 'Methods
+
+End Class
