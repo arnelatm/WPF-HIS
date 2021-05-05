9c06bbeee56c55f688147da3e06b15617760b726
Arnel Marcelo <arnelmarcelo@hotmail.com>
2021-05-05 13:49:25 +0300

    editing

---
+++ IspDataDb/dbo/Functions/FnGetAccountBalance.sql
@@ -0,0 +1,13 @@
+﻿
+
+
+
+CREATE FUNCTION [dbo].[FnGetAccountBalance](@IdNo Integer,@EndDate Date)
+RETURNS Decimal(16,2)
+Begin
+	Declare @Balance Decimal(16,2)
+	Declare @EndAcctCode Varchar(10)
+	Set @EndAcctCode = (Select AccountCode from Account where IdNo = @IdNo)
+	Set @Balance =  (Select Sum(debit-Credit) from [fnGetAcctActivityStatement](@EndDate,@EndDate,@EndAcctCode,@EndAcctCode))
+	return @Balance
+End
