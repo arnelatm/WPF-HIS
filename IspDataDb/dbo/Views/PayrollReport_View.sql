2f1abd7c82dfb1da8c3c300ceb53e31250a9e6f0
Arnel Marcelo <arnelmarcelo@hotmail.com>
2021-05-06 14:02:26 +0300

    editing

--- IspDataDb/dbo/Views/PayrollReport_View.sql
+++ IspDataDb/dbo/Views/PayrollReport_View.sql
@@ -3,12 +3,14 @@
 
 
 
+
 CREATE VIEW [dbo].[PayrollReport_View]
 AS
 SELECT       dbo.PayrollDetail.PayrollIdNo,dbo.PayrollDetail.EmployeeIdNo, Sum(IIf(dbo.PayElement.PayElementKind='E',dbo.PayrollPayELement.Amount,dbo.PayrollPayElement.Amount*-1)) as TotalAmount,
 				Sum(IIf(dbo.PayElement.PayElementKind='E',dbo.PayrollPayELement.Amount,0)) as TotalEarning, 
-				Sum(IIf(dbo.PayElement.PayElementKind<>'E',dbo.PayrollPayELement.Amount,0)) as TotalDeduction,dbo.PayElement.ReportGroupIdNo
+				Sum(IIf(dbo.PayElement.PayElementKind<>'E',dbo.PayrollPayELement.Amount,0)) as TotalDeduction,dbo.PayElement.ReportGroupIdNo,
+				PayElementIdNo
 FROM            dbo.PayElement RIGHT OUTER JOIN
                          dbo.PayrollPayElement INNER JOIN
                          dbo.PayrollDetail ON dbo.PayrollPayElement.PayrollDetailIdNo = dbo.PayrollDetail.IdNo ON dbo.PayElement.IdNo = dbo.PayrollPayElement.PayElementIdNo
-Group by payrollIdNo,EmployeeIdNo,ReportGroupIdNo
+Group by payrollIdNo,EmployeeIdNo,ReportGroupIdNo,PayElementIdNo
