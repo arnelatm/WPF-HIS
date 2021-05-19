cd229ac32c468c0459fa28a94054361d41a0faa0
Arnel Marcelo <arnelmarcelo@hotmail.com>
2021-05-19 13:34:27 +0300

    Renamed SalaryLoan Table to SalaryLoanSchedule

--- IspDataDb/dbo/Tables/SalaryLoan.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[SalaryLoan] (
-    [IdNo]          INT        NOT NULL,
-    [EmployeeIdNo]  INT        NOT NULL,
-    [Amount]        SMALLMONEY NULL,
-    [StartDate]     DATE       NULL,
-    [EndDate]       DATE       NULL,
-    [DateTimeStamp] ROWVERSION NULL
-);
-
---
+++ IspDataDb/dbo/Tables/SalaryLoanSchedule.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[SalaryLoanSchedule] (
+    [IdNo]            INT        IDENTITY (1, 1) NOT NULL,
+    [EmployeeIdNo]    INT        NULL,
+    [Amount]          SMALLMONEY NULL,
+    [StartDate]       DATE       NULL,
+    [PeriodicPayment] SMALLMONEY NULL,
+    [DateCreated]     DATETIME   NULL,
+    [DateTimeStamp]   ROWVERSION NULL,
+    CONSTRAINT [PK_SalaryLoan] PRIMARY KEY CLUSTERED ([IdNo] ASC)
+);
+
