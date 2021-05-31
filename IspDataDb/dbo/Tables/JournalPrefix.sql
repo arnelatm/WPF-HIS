a8d154ee96c540cbb7b7c6845a48a05e5e60f190
Arnel Marcelo <arnelmarcelo@hotmail.com>
2021-05-31 18:22:38 +0300

    Added Option for Journal Code Localization

---
+++ IspDataDb/dbo/Tables/JournalPrefix.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[JournalPrefix] (
+    [IdNo]           SMALLINT      IDENTITY (1, 1) NOT NULL,
+    [JournalCode]    CHAR (2)      NULL,
+    [JournalName]    VARCHAR (50)  NULL,
+    [JournalNameAra] NVARCHAR (50) NULL,
+    [JournalCodeAra] NVARCHAR (4)  NULL,
+    CONSTRAINT [PK_JournalPrefix] PRIMARY KEY CLUSTERED ([IdNo] ASC)
+);
+
