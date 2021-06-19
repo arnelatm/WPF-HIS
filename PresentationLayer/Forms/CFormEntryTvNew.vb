12f3cf2d0abc1cd9e7a383bf013ef854387d1295
Arnel Marcelo <arnelmarcelo@hotmail.com>
2021-06-19 13:50:48 +0300

    editing

--- PresentationLayer/Forms/CFormEntryTvNew.vb
+++ PresentationLayer/Forms/CFormEntryTvNew.vb
@@ -1,8 +1,10 @@
 ﻿Imports System.ComponentModel
 Imports System.Windows.Forms
 Imports AATM.Libraries.GlobalFuncNSub
+Imports AATM.Libraries.MessagingLibrary
 Imports AATM.PresentationLayer.Events
 Imports AATM.PresentationLayer.Presenters
+Imports AATM.PresentationLayer.Views
 
 Public Class CFormEntryTvNew
 
@@ -24,93 +26,60 @@ Public Class CFormEntryTvNew
 
     Public Property TreeViewData As New Object
 
-    Public Sub DisplayTree()
-        Dim root As TreeNode = FormTreeView.Nodes(0)
-        root.Nodes.Clear()
-        ' create the tree
-        If GlobalVariables.RightToLeftLayout Then
-            FormTreeView.RightToLeftLayout = True
-        Else
-            FormTreeView.RightToLeftLayout = False
-        End If
-        FormTreeView.RightToLeft = RightToLeft.Inherit
-        If ParentFieldName Is Nothing OrElse ParentFieldName = "" Then
-            For Each dataNode In TreeViewData
-                AddRecordToTree(dataNode)
-            Next
-        Else
-            For Each dataNode In TreeViewData
-                AddRecordToTreeHierarchical(dataNode, True)
-            Next
-        End If
-    End Sub
+    'Public Sub DisplayTree()
+    '    Dim root As TreeNode = FormTreeView.Nodes(0)
+    '    root.Nodes.Clear()
+    '    ' create the tree
+    '    If GlobalVariables.RightToLeftLayout Then
+    '        FormTreeView.RightToLeftLayout = True
+    '    Else
+    '        FormTreeView.RightToLeftLayout = False
+    '    End If
+    '    FormTreeView.RightToLeft = RightToLeft.Inherit
+    '    If ParentFieldName Is Nothing OrElse ParentFieldName = "" Then
+    '        For Each dataNode In TreeViewData
+    '            AddRecordToTree(dataNode)
+    '        Next
+    '    Else
+    '        For Each dataNode In TreeViewData
+    '            AddRecordToTreeHierarchical(dataNode, True)
+    '        Next
+    '    End If
+    'End Sub
 
-    Protected Overloads Sub AddRecordToTree(dataNode As Object) ', mainFieldName As String)
-        Dim idNo As Int32 = GetPropertyValue(dataNode, PresenterObj.IdFieldName)
-        Dim mainValue As String = GetPropertyValue(dataNode, "Name")
-        Dim secondaryValue As String = GetPropertyValue(dataNode, "Code")
-        Dim treeNode As TreeNode = MakeTreeNode(mainValue, secondaryValue, idNo)
-        FormTreeView.Nodes(0).Nodes.Add(treeNode)
-    End Sub
+    'Protected Overloads Sub AddRecordToTree(dataNode As Object) ', mainFieldName As String)
+    '    Dim idNo As Int32 = GetPropertyValue(dataNode, PresenterObj.IdFieldName)
+    '    Dim mainValue As String = GetPropertyValue(dataNode, "Name")
+    '    Dim secondaryValue As String = GetPropertyValue(dataNode, "Code")
+    '    Dim treeNode As TreeNode = MakeTreeNode(mainValue, secondaryValue, idNo)
+    '    FormTreeView.Nodes(0).Nodes.Add(treeNode)
+    'End Sub
 
-    Protected Overloads Sub AddRecordToTreeHierarchical(dataNode As Object, parentChanged As Boolean)
-        Dim parentIdValue As Integer? = GetPropertyValue(dataNode, ParentFieldName)
-        If parentIdValue Is Nothing OrElse parentIdValue = 0 Then
-            AddRecordToTree(dataNode) ', "Name")
-        Else
-            Dim idNo As Int32 = GetPropertyValue(dataNode, "IdNo")
-            Dim mainValue As String = GetPropertyValue(dataNode, "Name")
-            Dim secondaryValue As String = GetPropertyValue(dataNode, "Code")
-            Dim treeNode As TreeNode = MakeTreeNode(mainValue, secondaryValue, idNo)
-            If parentIdValue Is Nothing OrElse parentIdValue = 0 Then
-                If parentChanged Then
-                    FormTreeView.Nodes(FormTreeView.Nodes.Count - 1).Nodes.Add(treeNode)
-                Else
-                    FormTreeView.Nodes(0).Nodes.Add(treeNode)
-                End If
-            Else
-                If parentChanged Then
-                    Dim foundNode As TreeNode() = FormTreeView.Nodes.Find(parentIdValue.ToString(), True)
-                    If foundNode.Length <> 0 Then
-                        foundNode(0).Nodes.Add(treeNode)
-                    End If
-                End If
-            End If
-        End If
-    End Sub
-
-    Protected Sub BfTvEntry_AfterSelect(sender As Object, e As TreeViewEventArgs)
-
-        If Not _bypassSelectedChange Then
-            Select Case (e.Action)
-                Case TreeViewAction.ByKeyboard
-                    'MessageBox.Show("You like the keyboard!")
-                Case TreeViewAction.ByMouse
-                    'MessageBox.Show("You like the mouse!")
-                Case Else
-                    ' A problem here is causing a windows handle error when executing the below code.
-                    ' Therefore since this is just a selection change during initialization no need
-                    ' to execute the codes below so just exit the sub. This will also make initialization
-                    ' faster because no more need to move the database anyway at initialization the
-                    ' first record will be the one to be shown.
-                    Exit Sub
-            End Select
-            If Not PresenterObj.OkToMove() Then
-                Exit Sub
-            End If
-            Dim nTag As Integer
-            FormTreeView.ImageIndex = 1
-            If FormTreeView.SelectedNode.Tag.ToString = "root" Then
-                PresenterObj.RecordPositionNumber = 1
-            Else
-                nTag = FormTreeView.SelectedNode.Tag
-                PresenterObj.RecordPositionNumber = PresenterObj.GetSortedRecordPosition(nTag)
-            End If
-            If Not FormTreeView.SelectedNode.IsVisible Then
-                FormTreeView.SelectedNode.EnsureVisible()
-            End If
-        End If
-    End Sub
+    'Protected Overloads Sub AddRecordToTreeHierarchical(dataNode As Object, parentChanged As Boolean)
+    '    Dim parentIdValue As Integer? = GetPropertyValue(dataNode, ParentFieldName)
+    '    If parentIdValue Is Nothing OrElse parentIdValue = 0 Then
+    '        AddRecordToTree(dataNode) ', "Name")
+    '    Else
+    '        Dim idNo As Int32 = GetPropertyValue(dataNode, "IdNo")
+    '        Dim mainValue As String = GetPropertyValue(dataNode, "Name")
+    '        Dim secondaryValue As String = GetPropertyValue(dataNode, "Code")
+    '        Dim treeNode As TreeNode = MakeTreeNode(mainValue, secondaryValue, idNo)
+    '        If parentIdValue Is Nothing OrElse parentIdValue = 0 Then
+    '            If parentChanged Then
+    '                FormTreeView.Nodes(FormTreeView.Nodes.Count - 1).Nodes.Add(treeNode)
+    '            Else
+    '                FormTreeView.Nodes(0).Nodes.Add(treeNode)
+    '            End If
+    '        Else
+    '            If parentChanged Then
+    '                Dim foundNode As TreeNode() = FormTreeView.Nodes.Find(parentIdValue.ToString(), True)
+    '                If foundNode.Length <> 0 Then
+    '                    foundNode(0).Nodes.Add(treeNode)
+    '                End If
+    '            End If
+    '        End If
+    '    End If
+    'End Sub
 
     Public Sub DisplayTreeViewData()
         'Dim tvd As New TreeViewDisplay(TreeViewData)
@@ -119,39 +88,31 @@ Public Class CFormEntryTvNew
         End If
     End Sub
 
-    Protected Function MakeTreeNode(mainFieldValue As String, secondaryFieldValue As String, idNo As Int32) _
-        As TreeNode
-        Dim treeTextDisplay As String
-        treeTextDisplay = TreeNodeTextDisplay(mainFieldValue, secondaryFieldValue)
-        Return New TreeNode With {
-            .Text = treeTextDisplay,
-            .Tag = idNo,
-            .Name = idNo
-            }
-    End Function
+    'Protected Function MakeTreeNode(mainFieldValue As String, secondaryFieldValue As String, idNo As Int32) _
+    '    As TreeNode
+    '    Dim treeTextDisplay As String
+    '    treeTextDisplay = TreeNodeTextDisplay(mainFieldValue, secondaryFieldValue)
+    '    Return New TreeNode With {
+    '        .Text = treeTextDisplay,
+    '        .Tag = idNo,
+    '        .Name = idNo
+    '        }
+    'End Function
 
     Protected Overrides Sub OnTextDisplayLanguageChanged() Handles Me.TextDisplayLanguageChanged
         MyBase.OnTextDisplayLanguageChanged()
         DisplayTreeViewData()
     End Sub
 
-    Protected Overrides Sub RecordPositionChanged(ByRef e As RecordPositionChanged)
-        MyBase.RecordPositionChanged(e)
-        Debugger.Break()
-        GotoRecordInTreeView()
-    End Sub
+    'Protected Overrides Sub RecordPositionChanged(ByRef e As RecordPositionChanged)
+    '    MyBase.RecordPositionChanged(e)
+    '    Debugger.Break()
+    '    GotoRecordInTreeView()
+    'End Sub
 
-    Protected Overrides Sub RecordSaved(ByRef e As RecordSaved)
-        DisplayTreeViewData()
-    End Sub
-
-    Protected Sub RemoveCurrentNode(bypassChange As Boolean)
-        If bypassChange Then
-            _bypassSelectedChange = True
-        End If
-        FormTreeView.Nodes.Remove(FormTreeView.SelectedNode)
-        _bypassSelectedChange = False
-    End Sub
+    'Protected Overrides Sub RecordSaved(ByRef e As RecordSaved)
+    '    DisplayTreeViewData()
+    'End Sub
 
     Protected Overridable Function TreeNodeTextDisplay(tvName As String, ByVal Optional tvAdditionalText As String = "") _
         As String
@@ -166,6 +127,34 @@ Public Class CFormEntryTvNew
             FormTreeView.Refresh()
         End If
     End Sub
+
+    'Public Overridable Function GoDeleteRecord() As Integer
+    '    Dim record As New TM
+    '    GlobalVariables.Mapper.Map(Of IView, TM)(View, record)
+    '    Dim retValue = 0
+    '    Dim currentIdNo = CallByName(View, IdFieldName, CallType.Get)
+    '    If IsOkToDeleteRecord() Then
+    '        If Messaging.Show(True, "AskIfDeleteRecord", "Are you sure you want to delete this record?", "Please Confirm Delete!", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2) = DialogResult.Yes Then
+    '            RaiseEvent BeforeDelete()
+    '            retValue = DeleteRecord(currentIdNo)
+    '            If retValue <= 0 Then
+    '                Messaging.Show(True, "MsgDeleteRecordFailed", "This record was not deleted because of an error. Please try again later or ask Database Administrator for help.", "Deletion Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+    '            Else
+    '                Messaging.Show(True, "MsgRecordSuccessfullyDeleted", "Record was successfully deleted.", "Record Deleted")
+    '                ' if deleted stay on that given RecordPositionNumber
+    '                ' which in this case will be the next record after the deleted record
+    '                TargetIdNo = GetIdNoOfSortedPositionNumber(RecordPositionNumber)
+    '                If TargetIdNo = 0 Then
+    '                    ' last record deleted
+    '                    GoLastRecord()
+    '                End If
+    '                UpdateViewDisplay(TargetIdNo)
+    '            End If
+    '            RaiseEvent AfterDelete()
+    '        End If
+    '    End If
+    '    Return retValue
+    'End Function
 
     Private Sub BfTvEntry_Shown(sender As Object, e As EventArgs) Handles MyBase.Shown
         _bypassSelectedChange = True
