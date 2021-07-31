5cd2f86f3eeca4375d3213c2901c2777f7e78e2a
Arnel Marcelo <arnelmarcelo@hotmail.com>
2021-07-31 17:27:14 +0300

    update tables

--- IspDataDb/dbo/Tables/RecurringPayElement.sql
+++ IspDataDb/dbo/Tables/RecurringPayElement.sql
@@ -1,15 +1,12 @@
-﻿CREATE TABLE [dbo].[SalaryLoanSchedule] (
+﻿CREATE TABLE [dbo].[RecurringPayElement] (
     [IdNo]            INT        IDENTITY (1, 1) NOT NULL,
     [EmployeeIdNo]    INT        NULL,
     [Amount]          MONEY      NULL,
     [StartDate]       DATE       NULL,
+    [PayElementIdNo]  SMALLINT   NULL,
     [PeriodicPayment] MONEY      NULL,
     [DateCreated]     DATETIME   CONSTRAINT [DF_SalaryLoanSchedule_DateCreated] DEFAULT (getdate()) NULL,
     [DateTimeStamp]   ROWVERSION NULL,
     CONSTRAINT [PK_SalaryLoan] PRIMARY KEY CLUSTERED ([IdNo] ASC)
 );
-
-
-
-
 
