60dd24892fdd7b2c381b9205e31758dfaee47f4a
Arnel Marcelo <arnelmarcelo@hotmail.com>
2021-08-03 18:47:33 +0300

    Update Tables

---
+++ IspDataDb/dbo/Views/PayrollCsvReport_View.sql
@@ -0,0 +1,33 @@
+﻿
+
+
+
+
+
+
+CREATE VIEW [dbo].[PayrollCsvReport_View]
+AS
+SELECT      dbo.PayrollDetail.PayrollIdNo as 'PayrollIdNo',
+			dbo.Bank.BankCode as 'BankName',
+			dbo.Employee.IBAN as 'AcctNo',
+			Sum(IIf(dbo.PayElement.PayElementKind='E',dbo.PayrollPayELement.Amount,dbo.PayrollPayElement.Amount*-1)) as 'NetPay',
+			'' as 'Notes',
+			dbo.Employee.EmployeeName as 'EmpName',
+			dbo.Employee.NationalIdNo as 'IqamaNo',
+			'Jeddah' as 'Address',
+			Sum(Iif(dbo.PayElementGroup.PayElementGroupCode = 'BP',dbo.PayrollPayElement.Amount,0)) as 'SalaryEr',
+			Sum(Iif(dbo.PayElementGroup.PayElementGroupCode  = 'HA',dbo.PayrollPayElement.Amount,0)) as 'Housing',
+			Sum(IIf(dbo.PayElement.PayElementKind='E' and dbo.PayElementGroup.PayElementGroupCode <>'BP' and dbo.PayElementGroup.PayElementGroupCode <>'HA',dbo.PayrollPayELement.Amount,0)) as 'OtherWage',
+			Sum(IIf(dbo.PayElement.PayElementKind='D',dbo.PayrollPayELement.Amount,0)) as 'Deductions'
+FROM        dbo.PayrollPayElement
+			Left Join dbo.PayElement 
+			on dbo.PayrollPayElement.PayElementIdNo = dbo.PayElement.IdNo
+			INNER JOIN dbo.PayrollDetail 
+			ON dbo.PayrollPayElement.PayrollDetailIdNo = dbo.PayrollDetail.IdNo 
+			INNER JOIN dbo.Employee
+			ON dbo.PayrollDetail.EmployeeIdNo = dbo.Employee.IdNo
+			LEFT JOIN dbo.Bank
+			On dbo.Employee.BankIdNo = dbo.Bank.IdNo
+			LEFT JOIN dbo.PayElementGroup
+			on dbo.PayElement.ReportGroupIdNo = dbo.PayElementGroup.IdNo
+Group by dbo.PayrollDetail.PayrollIdNo,payrollIdNo,EmployeeIdNo,IBAN,ReportGroupIdNo,dbo.Bank.BankCode,dbo.Employee.BankAccountNo,dbo.Employee.EmployeeName,dbo.Employee.NationalIdNo
