7502949c1b7d0431d4f0d64a5c8a1ad62dacd1c4
Arnel Marcelo <arnelmarcelo@hotmail.com>
2021-08-04 18:07:06 +0300

    Update EMployeeLeave Table

--- IspDataDb/dbo/Tables/EmployeeLeave.sql
+++ IspDataDb/dbo/Tables/EmployeeLeave.sql
@@ -2,8 +2,8 @@
     [IdNo]          INT            IDENTITY (1, 1) NOT NULL,
     [EmployeeIdNo]  INT            NULL,
     [LeaveIdNo]     SMALLINT       NOT NULL,
-    [LeaveStart]    DATETIME       NULL,
-    [LeaveEnd]      DATETIME       NULL,
+    [StartDate]     DATETIME       NULL,
+    [EndDate]       DATETIME       NULL,
     [FullDayLeave]  BIT            NULL,
     [LeaveStatus]   CHAR (1)       NULL,
     [LeaveReason]   NVARCHAR (200) NULL,
@@ -11,6 +11,8 @@
     [DateTimeStamp] ROWVERSION     NULL,
     CONSTRAINT [PK_EmployeeLeave] PRIMARY KEY CLUSTERED ([IdNo] ASC)
 );
+
+
 
 
 
