9d5b7111bcf43d7afa5f64427e953895c1caf7d0
Arnel Marcelo <arnelmarcelo@hotmail.com>
2021-09-19 21:43:04 +0300

    Editing

---
+++ Accounts/BusinessLayer/EmployeeLeaveCredit.vb
@@ -0,0 +1,32 @@
+﻿' Category business object
+' ** Enterprise Design Pattern: Domain Model, Identity Field
+Imports AATM.BusinessLayer.BusinessRules
+
+Namespace BusinessLayer
+
+    Public Class EmployeeLeaveCredit
+        Inherits AATM.BusinessLayer.BusinessObject
+
+        ' ** Enterprise Design Pattern: Identity field pattern
+        Public Sub New()
+            ' establish business rules
+            If GetRules().Count() = 0 Then
+                AddRule(New ValidateRequired("EmployeeIdNo"))
+                AddRule(New ValidateRequired("LeaveIdNo"))
+            End If
+        End Sub
+
+        Public Property IdNo As Int16
+        Public Property EmployeeIdNo As Int32
+        Public Property LeaveIdNo As Int16
+        Public Property LeaveAllowed As Int16
+        Public Property PaidPercent As Decimal
+        Public Property Cumulative As Boolean
+        Public Property MaxCarryOver As Int16
+        Public Property MaxLimit As Int16
+        Public Property NoMaxLimit As Boolean
+        Public Property AccumulatedLeaves As Int16
+
+    End Class
+
+End Namespace
---
+++ Accounts/PresentationLayer/Models/EmployeeLeaveCreditModel.vb
@@ -0,0 +1,23 @@
+﻿Namespace PresentationLayer.Models
+
+    ''' <summary>
+    '''     The Model in MVP design pattern.
+    '''     Implements IModel and communicates with WCF Service.
+    ''' </summary>
+    Public Class EmployeeLeaveCreditModel
+
+        Public Property Errors As List(Of String)
+        Public Property IdNo As Int16
+        Public Property EmployeeIdNo As Int32
+        Public Property LeaveIdNo As Int16
+        Public Property LeaveAllowed As Int16
+        Public Property PaidPercent As Decimal
+        Public Property Cumulative As Boolean
+        Public Property MaxCarryOver As Int16
+        Public Property MaxLimit As Int16
+        Public Property NoMaxLimit As Boolean
+        Public Property AccumulatedLeaves As Int16
+
+    End Class
+
+End Namespace
