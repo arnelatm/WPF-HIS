6ec6edcfd3a225639d195bd76ed22580c4936136
Arnel Marcelo <arnelmarcelo@hotmail.com>
2021-09-29 17:15:29 +0300

    update tables

--- IspDataDb/dbo/Tables/PayrollDetail.sql
+++ IspDataDb/dbo/Tables/PayrollDetail.sql
@@ -2,9 +2,12 @@
     [IdNo]          INT        IDENTITY (1, 1) NOT NULL,
     [PayrollIdNo]   SMALLINT   NULL,
     [EmployeeIdNo]  INT        NULL,
+    [BankTransfer]  BIT        NULL,
     [DateTimeStamp] ROWVERSION NULL,
     CONSTRAINT [PK_PayrollDetail] PRIMARY KEY CLUSTERED ([IdNo] ASC)
 );
+
+
 
 
 
