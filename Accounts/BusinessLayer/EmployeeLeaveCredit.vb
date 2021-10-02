aa59168c415f2eba2fa95f0694eaabf8266596e1
Arnel Marcelo <arnelmarcelo@hotmail.com>
2021-10-02 11:54:45 +0300

    Editing

--- Accounts/BusinessLayer/EmployeeLeaveCredit.vb
+++ Accounts/BusinessLayer/EmployeeLeaveCredit.vb
@@ -16,16 +16,17 @@ Namespace BusinessLayer
             End If
         End Sub
 
-        Public Property IdNo As Int16
-        Public Property EmployeeIdNo As Int32
-        Public Property LeaveIdNo As Int16
-        Public Property LeaveAllowed As Int16
-        Public Property PaidPercent As Decimal
+        Public Property AccumulatedLeaves As Int16
         Public Property Cumulative As Boolean
+        Public Property EmployeeIdNo As Int32
+        Public Property IdNo As Int16
+        Public Property LeaveAllowed As Int16
+        Public Property LeaveIdNo As Int16
         Public Property MaxCarryOver As Int16
         Public Property MaxLimit As Int16
         Public Property NoMaxLimit As Boolean
-        Public Property AccumulatedLeaves As Int16
+        Public Property PaidPercent As Decimal
+        Public Property Sequence As Int16
 
     End Class
 
