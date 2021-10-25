5a4a335f90668d01eade9cea5391622e1abe5f1d
Arnel Marcelo <arnelmarcelo@hotmail.com>
2021-10-25 17:52:11 +0300

    Added ShiftSummary_View

--- IspDataDb/dbo/Tables/ShiftSummary.sql
+++ IspDataDb/dbo/Tables/ShiftSummary.sql
@@ -1,10 +1,14 @@
 ﻿CREATE TABLE [dbo].[ShiftSummary] (
-    [IdNo]      INT             IDENTITY (1, 1) NOT NULL,
-    [UserIdNo]  INT             NULL,
-    [DateStart] DATE            NULL,
-    [DateEnd]   DATE            NULL,
-    [Cash]      DECIMAL (10, 2) NULL,
-    [Cards]     DECIMAL (10, 2) NULL,
+    [IdNo]          INT             IDENTITY (1, 1) NOT NULL,
+    [UserIdNo]      INT             NOT NULL,
+    [DateStart]     DATETIME        NOT NULL,
+    [DateEnd]       DATETIME        NOT NULL,
+    [Cash]          DECIMAL (10, 2) NULL,
+    [Cards]         DECIMAL (10, 2) NULL,
+    [DateCreated]   DATETIME        CONSTRAINT [DF_ShiftSummary_DateCreated] DEFAULT (getdate()) NULL,
+    [DateTimeStamp] ROWVERSION      NULL,
     CONSTRAINT [PK_ShiftSummary] PRIMARY KEY CLUSTERED ([IdNo] ASC)
 );
+
+
 
