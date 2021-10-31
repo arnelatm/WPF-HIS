c2034d7e5f648d715fe9648d3132a22a4f5235ac
Arnel Marcelo <arnelmarcelo@hotmail.com>
2021-10-31 16:56:15 +0300

    Adding Readng Image

---
+++ Libraries/Image/Classes/Employee.vb
@@ -0,0 +1,9 @@
+﻿Public Class Employee
+    Public Property IdNo() As Integer
+    Public Property EmployeeName() As String
+    Public Property Picture() As Image
+
+    Public Overrides Function ToString() As String
+        Return EmployeeName
+    End Function
+End Class
