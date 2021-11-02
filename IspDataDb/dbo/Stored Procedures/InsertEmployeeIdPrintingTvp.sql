a07f6d91dc06e78b4ab18fdb9436d5900dab3fef
Arnel Marcelo <arnelmarcelo@hotmail.com>
2021-11-02 21:45:35 +0300

    Added Table for Printed Employee ID

---
+++ IspDataDb/dbo/Stored Procedures/InsertEmployeeIdPrintingTvp.sql
@@ -0,0 +1,18 @@
+﻿
+
+
+
+
+
+
+
+
+
+
+CREATE PROC [dbo].[InsertEmployeeIdPrintingTvp]
+  @MParam EmployeeIdPrintingInsert READONLY
+AS 
+INSERT  INTO EmployeeIdPrinting (EmployeeIdNo, TransactionNumber)
+        SELECT  EmployeeIdNo, TransactionNumber
+        FROM    @MParam
+SET IDENTITY_INSERT DBO.EmployeeIdPrinting ON;
