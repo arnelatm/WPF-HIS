77096b4ea4046e671eb18c6f462efe4d39f314a0
Arnel Marcelo <arnelmarcelo@hotmail.com>
2021-11-13 11:04:13 +0300

    editing employeeleavestatusupdate views

---
+++ IspDataDb/dbo/Views/EmployeeLeaveLatestUpdate_View.sql
@@ -0,0 +1,28 @@
+﻿
+
+CREATE View [dbo].[EmployeeLeaveLatestUpdate_View] As
+SELECT TOP (1000) [IdNo]
+      ,[AppliedBy]
+      ,[LeaveIdNo]
+      ,[StartDate]
+      ,[EndDate]
+      ,[FullDay]
+      ,[EnteredBy]
+      ,[Status]
+      ,[Note]
+      ,[LeaveStatusDate]
+	  ,[LatestStatusUpdate]
+  FROM [ISPDATA].[dbo].[EmployeeLeaveStatus_View]
+  Union
+  SELECT [IdNo]
+      ,[AppliedBy]
+      ,[LeaveIdNo]
+      ,[StartDate]
+      ,[EndDate]
+      ,[FullDay]
+      ,[EnteredBy]
+      ,[Status]
+      ,[Note]
+      ,[DateCreated]
+	  ,[LeaveStatusDate]
+ FROM EmployeeLeave_View b where b.LeaveStatusIdNo is Null
--- IspDataDb/dbo/Views/EmployeeLeaveStatusList_View.sql
+++ IspDataDb/dbo/Views/EmployeeLeaveStatusList_View.sql
@@ -1,6 +1,8 @@
-﻿Create View EmployeeLeaveStatusList_View
+﻿
+
+CREATE View [dbo].[EmployeeLeaveStatusList_View]
 As 
-SELECT  a.IdNo, a.AppliedBy, a.LeaveIdNo, a.StartDate, a.EndDate, a.FullDay, b.EnteredBy, b.[Status], b.Note, b.DateCreated 
+SELECT  a.IdNo, a.AppliedBy, a.LeaveIdNo, a.StartDate, a.EndDate, a.FullDay, b.EnteredBy, b.[Status], b.Note, b.DateCreated as LeaveStatusDate
 from EmployeeLeave a  
 Left Join EmployeeLeaveStatus b 
 On a.IdNo  = b.EmployeeLeaveIdNo
