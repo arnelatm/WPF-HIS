bc7f4946f5e9d6b982ee0b54ef936f4841648e69
Arnel Marcelo <arnelmarcelo@hotmail.com>
2021-11-20 13:21:19 +0300

    Updated Tables

--- IspDataDb/dbo/Views/EmployeeLeaveStatus_View.sql
+++ IspDataDb/dbo/Views/EmployeeLeaveStatus_View.sql
@@ -1,10 +1,155 @@
-﻿
-CREATE View [dbo].[EmployeeLeaveStatus_View] 
+﻿CREATE VIEW dbo.EmployeeLeaveStatus_View
 AS
-SELECT *
-FROM EmployeeLeaveStatusList_View a
-INNER JOIN
-    (SELECT EmployeeLeaveIdNo, MAX(DateCreated) AS LatestStatusUpdate
-    FROM EmployeeLeaveStatus
-    GROUP BY EmployeeLeaveIdNo) b
-ON a.IdNo = b.EmployeeLeaveIdNo AND a.LeaveStatusDate = b.LatestStatusUpdate
+SELECT        a.IdNo, a.AppliedBy, a.LeaveIdNo, a.StartDate, a.EndDate, a.FullDay, a.EnteredBy, a.Status, a.Note, a.LeaveStatusDate, b.EmployeeLeaveIdNo, b.LatestStatusUpdate, dbo.EmployeeLeave.DateCreated, 
+                         dbo.EmployeeLeave.EmployeeIdNo, dbo.EmployeeLeave.LeaveReason, dbo.Employee.SupervisorIdNo
+FROM            dbo.EmployeeLeaveStatusList_View AS a INNER JOIN
+                             (SELECT        EmployeeLeaveIdNo, MAX(DateCreated) AS LatestStatusUpdate
+                               FROM            dbo.EmployeeLeaveStatus
+                               GROUP BY EmployeeLeaveIdNo) AS b ON a.IdNo = b.EmployeeLeaveIdNo AND a.LeaveStatusDate = b.LatestStatusUpdate INNER JOIN
+                         dbo.EmployeeLeave ON a.IdNo = dbo.EmployeeLeave.IdNo LEFT OUTER JOIN
+                         dbo.Employee ON dbo.EmployeeLeave.EmployeeIdNo = dbo.Employee.IdNo
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_DiagramPaneCount', @value = 1, @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'VIEW', @level1name = N'EmployeeLeaveStatus_View';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_DiagramPane1', @value = N'[0E232FF0-B466-11cf-A24F-00AA00A3EFFF, 1.00]
+Begin DesignProperties = 
+   Begin PaneConfigurations = 
+      Begin PaneConfiguration = 0
+         NumPanes = 4
+         Configuration = "(H (1[40] 4[20] 2[20] 3) )"
+      End
+      Begin PaneConfiguration = 1
+         NumPanes = 3
+         Configuration = "(H (1 [50] 4 [25] 3))"
+      End
+      Begin PaneConfiguration = 2
+         NumPanes = 3
+         Configuration = "(H (1 [50] 2 [25] 3))"
+      End
+      Begin PaneConfiguration = 3
+         NumPanes = 3
+         Configuration = "(H (4 [30] 2 [40] 3))"
+      End
+      Begin PaneConfiguration = 4
+         NumPanes = 2
+         Configuration = "(H (1 [56] 3))"
+      End
+      Begin PaneConfiguration = 5
+         NumPanes = 2
+         Configuration = "(H (2 [66] 3))"
+      End
+      Begin PaneConfiguration = 6
+         NumPanes = 2
+         Configuration = "(H (4 [50] 3))"
+      End
+      Begin PaneConfiguration = 7
+         NumPanes = 1
+         Configuration = "(V (3))"
+      End
+      Begin PaneConfiguration = 8
+         NumPanes = 3
+         Configuration = "(H (1[56] 4[18] 2) )"
+      End
+      Begin PaneConfiguration = 9
+         NumPanes = 2
+         Configuration = "(H (1 [75] 4))"
+      End
+      Begin PaneConfiguration = 10
+         NumPanes = 2
+         Configuration = "(H (1[66] 2) )"
+      End
+      Begin PaneConfiguration = 11
+         NumPanes = 2
+         Configuration = "(H (4 [60] 2))"
+      End
+      Begin PaneConfiguration = 12
+         NumPanes = 1
+         Configuration = "(H (1) )"
+      End
+      Begin PaneConfiguration = 13
+         NumPanes = 1
+         Configuration = "(V (4))"
+      End
+      Begin PaneConfiguration = 14
+         NumPanes = 1
+         Configuration = "(V (2))"
+      End
+      ActivePaneConfig = 0
+   End
+   Begin DiagramPane = 
+      Begin Origin = 
+         Top = 0
+         Left = 0
+      End
+      Begin Tables = 
+         Begin Table = "a"
+            Begin Extent = 
+               Top = 6
+               Left = 38
+               Bottom = 320
+               Right = 213
+            End
+            DisplayFlags = 280
+            TopColumn = 0
+         End
+         Begin Table = "b"
+            Begin Extent = 
+               Top = 6
+               Left = 251
+               Bottom = 305
+               Right = 448
+            End
+            DisplayFlags = 280
+            TopColumn = 0
+         End
+         Begin Table = "EmployeeLeave"
+            Begin Extent = 
+               Top = 6
+               Left = 486
+               Bottom = 304
+               Right = 659
+            End
+            DisplayFlags = 280
+            TopColumn = 0
+         End
+         Begin Table = "Employee"
+            Begin Extent = 
+               Top = 6
+               Left = 697
+               Bottom = 294
+               Right = 895
+            End
+            DisplayFlags = 280
+            TopColumn = 0
+         End
+      End
+   End
+   Begin SQLPane = 
+   End
+   Begin DataPane = 
+      Begin ParameterDefaults = ""
+      End
+   End
+   Begin CriteriaPane = 
+      Begin ColumnWidths = 11
+         Column = 1440
+         Alias = 900
+         Table = 1170
+         Output = 720
+         Append = 1400
+         NewValue = 1170
+         SortType = 1350
+         SortOrder = 1410
+         GroupBy = 1350
+         Filter = 1350
+         Or = 1350
+         Or = 1350
+         Or = 1350
+      End
+   End
+End
+', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'VIEW', @level1name = N'EmployeeLeaveStatus_View';
+
