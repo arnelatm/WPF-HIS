1aec296636147e2ee938943b81f1dea752d663be
Arnel Marcelo <arnelmarcelo@hotmail.com>
2021-11-24 09:53:15 +0300

    Editing

--- Accounts/DataLayer/AdoNet/EmployeeLeaveApprovalDao.vb
+++ Accounts/DataLayer/AdoNet/EmployeeLeaveApprovalDao.vb
@@ -91,46 +91,31 @@ Namespace DataLayer.AdoNet
             }
 
         Private Function Take(employeeLeaveApproval As EmployeeLeaveApproval) As Object()
-            Return New Object() {
-                                    "@EnteredBy", employeeLeaveApproval.EnteredBy,
-                                    "@IdNo", employeeLeaveApproval.IdNo
-                                    }
+            Return New Object() {"@EnteredBy", employeeLeaveApproval.EnteredBy,
+                                 "@IdNo", employeeLeaveApproval.IdNo
+                                }
         End Function
 
         Public Function GetLeaveHistory(ByVal employeeLeaveIdNo As Int32) As List(Of EmployeeLeaveApprovalHistory)
             Dim sql As String = "SELECT " &
-                                "AppliedBy," &
                                 "DateCreated," &
-                                "EmployeeIdNo," &
-                                "EndDate," &
-                                "FullDay," &
-                                "IdNo," &
-                                "LeaveIdNo," &
-                                "LeaveReason," &
-                                "LeaveStatus," &
-                                "StartDate," &
                                 "EnteredBy," &
+                                "IdNo," &
                                 "Note," &
-                                "Status," &
+                                "Status" &
                                 " FROM EmployeeLeaveApproval_View" &
-                                " WHERE EmployeeLeaveIdNo = " &
-            Return _db.Read(sql, MakeApproval).ToList()
+                                " WHERE EmployeeLeaveIdNo = " & employeeLeaveIdNo.ToString()
+            Return _db.Read(sql, MakeApprovalHistory).ToList()
         End Function
 
         Private Shared ReadOnly MakeApprovalHistory As Func(Of IDataReader, EmployeeLeaveApprovalHistory) =
                                     Function(reader) _
-            New EmployeeLeave() With {
-            .AppliedBy = AATM.DataLayer.AdoNet.Extensions.AsId(Of Int32)(reader("AppliedBy")),
+            New EmployeeLeaveApprovalHistory() With {
             .DateCreated = AATM.DataLayer.AdoNet.Extensions.AsNullableDateTime(reader("DateCreated")),
-            .EmployeeIdNo = AATM.DataLayer.AdoNet.Extensions.AsId(Of Int32)(reader("EmployeeIdNo")),
-            .EndDate = AATM.DataLayer.AdoNet.Extensions.AsDateTime(reader("EndDate")),
-            .FullDay = AATM.DataLayer.AdoNet.Extensions.AsBool(reader("FullDay")),
+            .EnteredBy = AATM.DataLayer.AdoNet.Extensions.AsId(Of Int32)(reader("EnteredBy")),
             .IdNo = AATM.DataLayer.AdoNet.Extensions.AsId(Of Int32)(reader("IdNo")),
-            .LeaveIdNo = AATM.DataLayer.AdoNet.Extensions.AsInt(Of Int16)(reader("LeaveIdNo")),
-            .LeaveReason = AATM.DataLayer.AdoNet.Extensions.AsString(reader("LeaveReason")),
-            .LeaveStatus = AATM.DataLayer.AdoNet.Extensions.AsString(reader("LeaveStatus")),
-            .StartDate = AATM.DataLayer.AdoNet.Extensions.AsDateTime(reader("StartDate")),
-            .SupervisorIdNo = AATM.DataLayer.AdoNet.Extensions.AsInt(Of Int32)(reader("SupervisorIdNo"))
+            .Status = AATM.DataLayer.AdoNet.Extensions.AsString(reader("Status")),
+            .Note = AATM.DataLayer.AdoNet.Extensions.AsString(reader("Note"))
             }
 
     End Class
