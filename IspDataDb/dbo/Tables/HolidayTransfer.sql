d77a8117f9c7cb59433d72998d6202dc85c5738c
Arnel Marcelo <arnelmarcelo@hotmail.com>
2021-11-25 15:54:04 +0300

    Editing

---
+++ IspDataDb/dbo/Tables/HolidayTransfer.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[HolidayTransfer] (
+    [IdNo]          INT        NULL,
+    [HolidayIdNo]   INT        NULL,
+    [EmployeeIdNo]  INT        NULL,
+    [AppliedBy]     INT        NULL,
+    [DateCreated]   DATE       NULL,
+    [DateTimeStamp] ROWVERSION NULL
+);
+
