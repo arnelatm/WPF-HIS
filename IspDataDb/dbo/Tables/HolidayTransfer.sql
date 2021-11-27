cc996eac31d6e08b3e9a359c513ab4b23f4182b4
Arnel Marcelo <arnelmarcelo@hotmail.com>
2021-11-27 21:31:42 +0300

    Editing Tables

--- IspDataDb/dbo/Tables/HolidayTransfer.sql
+++ IspDataDb/dbo/Tables/HolidayTransfer.sql
@@ -1,9 +1,11 @@
 ﻿CREATE TABLE [dbo].[HolidayTransfer] (
-    [IdNo]          INT        NULL,
+    [IdNo]          INT        IDENTITY (1, 1) NOT NULL,
     [HolidayIdNo]   INT        NULL,
-    [EmployeeIdNo]  INT        NULL,
     [AppliedBy]     INT        NULL,
-    [DateCreated]   DATE       NULL,
-    [DateTimeStamp] ROWVERSION NULL
+    [DateCreated]   DATE       CONSTRAINT [DF_HolidayTransfer_DateCreated] DEFAULT (getdate()) NULL,
+    [DateTimeStamp] ROWVERSION NULL,
+    CONSTRAINT [PK_HolidayTransfer] PRIMARY KEY CLUSTERED ([IdNo] ASC)
 );
+
+
 
---
+++ IspDataDb/dbo/Tables/HolidayTransferItem.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[HolidayTransferItem] (
+    [IdNo]                INT IDENTITY (1, 1) NOT NULL,
+    [HolidayTransferIdNo] INT NULL,
+    [EmployeeIdNo]        INT NULL,
+    CONSTRAINT [PK_HolidayTransferItem] PRIMARY KEY CLUSTERED ([IdNo] ASC)
+);
+
