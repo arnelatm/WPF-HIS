08174124dc4baedc0be1a004c218e9a9d8642126
Arnel Marcelo <arnelmarcelo@hotmail.com>
2021-11-27 21:31:42 +0300

    Editing Tables

--- IspDataDb/dbo/Tables/HolidayTransferAvailment.sql
+++ IspDataDb/dbo/Tables/HolidayTransferAvailment.sql
@@ -1,7 +1,12 @@
 ﻿CREATE TABLE [dbo].[HolidayTransferAvailment] (
-    [IdNo]                INT  NULL,
-    [HolidayTransferIdNo] INT  NULL,
-    [DateStart]           DATE NULL,
-    [DateEnd]             DATE NULL
+    [IdNo]                INT        IDENTITY (1, 1) NOT NULL,
+    [HolidayTransferIdNo] INT        NULL,
+    [DateAvailed]         DATE       NULL,
+    [EnteredBy]           INT        NULL,
+    [DateCreated]         DATE       CONSTRAINT [DF_HolidayTransferAvailment_DateCreated] DEFAULT (getdate()) NULL,
+    [DateTimeStamp]       ROWVERSION NULL,
+    CONSTRAINT [PK_HolidayTransferAvailment] PRIMARY KEY CLUSTERED ([IdNo] ASC)
 );
+
+
 
