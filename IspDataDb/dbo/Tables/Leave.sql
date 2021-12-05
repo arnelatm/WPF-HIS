00d520ed188a417e82ce566358575b9385ba9f3a
Arnel Marcelo <arnelmarcelo@hotmail.com>
2021-12-05 08:34:22 +0300

    Added holiday field to Leave

--- IspDataDb/dbo/Tables/Leave.sql
+++ IspDataDb/dbo/Tables/Leave.sql
@@ -3,15 +3,18 @@
     [LeaveCode]     VARCHAR (3)    NULL,
     [LeaveName]     VARCHAR (100)  NOT NULL,
     [LeaveNameAra]  NVARCHAR (100) NOT NULL,
-    [LeaveAllowed]  DECIMAL(6, 2)       NULL,
+    [LeaveAllowed]  DECIMAL (6, 2) NULL,
     [PaidPercent]   DECIMAL (6, 2) NULL,
-    [MaxCarryOver]  DECIMAL(6, 2)       NULL,
+    [MaxCarryOver]  DECIMAL (6, 2) NULL,
+    [Holiday]       BIT            NULL,
     [Cumulative]    BIT            NULL,
     [MaxLimit]      DECIMAL (7, 2) NULL,
     [Notes]         NVARCHAR (200) NULL,
     [DateTimeStamp] ROWVERSION     NULL,
     CONSTRAINT [PK_AbsenceLeave] PRIMARY KEY CLUSTERED ([IdNo] ASC)
 );
+
+
 
 
 
