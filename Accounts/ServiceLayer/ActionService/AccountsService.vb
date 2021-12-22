4f01d7ce0af902a1a29a4dd77b110e5578f1b5fc
Arnel Marcelo <arnelmarcelo@hotmail.com>
2021-12-22 08:58:49 +0300

    Editing Employee Leave Selection

--- Accounts/ServiceLayer/ActionService/AccountsService.vb
+++ Accounts/ServiceLayer/ActionService/AccountsService.vb
@@ -141,8 +141,8 @@ Namespace ServiceLayer.ActionService
             Return model
         End Function
 
-        Friend Function GetEmployeeLeaves(idNo As Integer, leaveIdNo As Short) As List(Of EmployeeLeaveModel)
-            Dim records = DataDao.GetEmployeeLeaves(idNo, leaveIdNo)
+        Friend Function GetEmployeeLeaves(idNo As Integer, leaveIdNo As Short, Optional filterSelection As String = "") As List(Of EmployeeLeaveModel)
+            Dim records = DataDao.GetEmployeeLeaves(idNo, leaveIdNo, filterSelection)
             Dim model As New List(Of EmployeeLeaveModel)
             GlobalVariables.Mapper.Map(records, model)
             Return model
