d88fbdf7bdea1ccc9843af13e5ebacd6b2d6175a
Arnel Marcelo <arnelmarcelo@hotmail.com>
2021-12-23 20:32:21 +0300

    Editing

--- IspDataDb/dbo/User Defined Types/EmployeeLeaveCreditInsert.sql
+++ IspDataDb/dbo/User Defined Types/EmployeeLeaveCreditInsert.sql
@@ -1,14 +1,16 @@
 ﻿CREATE TYPE [dbo].[EmployeeLeaveCreditInsert] AS TABLE (
     [AccumulatedLeave] DECIMAL (8, 2) NULL,
-    [Cumulative]       BIT            NOT NULL,
+    [Cumulative]       BIT            NULL,
     [EmployeeIdNo]     INT            NOT NULL,
     [LeaveAllowed]     DECIMAL (6, 2) NULL,
     [LeaveIdNo]        SMALLINT       NOT NULL,
     [MaxCarryOver]     DECIMAL (6, 2) NULL,
     [MaxLimit]         DECIMAL (7, 2) NULL,
-    [NoMaxLimit]       BIT            NOT NULL,
+    [NoMaxLimit]       BIT            NULL,
     [PaidPercent]      DECIMAL (5, 2) NULL,
     [Sequence]         SMALLINT       NOT NULL);
+
+
 
 
 
--- IspDataDb/dbo/User Defined Types/EmployeeLeaveCreditUpdate.sql
+++ IspDataDb/dbo/User Defined Types/EmployeeLeaveCreditUpdate.sql
@@ -1,15 +1,17 @@
 ﻿CREATE TYPE [dbo].[EmployeeLeaveCreditUpdate] AS TABLE (
     [AccumulatedLeave] DECIMAL (8, 2) NULL,
-    [Cumulative]       BIT            NOT NULL,
+    [Cumulative]       BIT            NULL,
     [EmployeeIdNo]     INT            NOT NULL,
     [IdNo]             INT            NOT NULL,
     [LeaveAllowed]     DECIMAL (6, 2) NULL,
     [LeaveIdNo]        SMALLINT       NOT NULL,
     [MaxCarryOver]     DECIMAL (6, 2) NULL,
     [MaxLimit]         DECIMAL (7, 2) NULL,
-    [NoMaxLimit]       BIT            NOT NULL,
-    [PaidPercent]      DECIMAL (5, 2) NOT NULL,
+    [NoMaxLimit]       BIT            NULL,
+    [PaidPercent]      DECIMAL (5, 2) NULL,
     [Sequence]         SMALLINT       NOT NULL);
+
+
 
 
 
