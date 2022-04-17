ae43129d11118af4444c5216bc16aa53f943f588
Arnel Marcelo <arnelmarcelo@hotmail.com>
2022-04-17 23:59:11 +0300

    Editing

--- IspDataDb/dbo/Views/GeneralJournalItem_View.sql
+++ IspDataDb/dbo/Views/GeneralJournalItem_View.sql
@@ -1,145 +1,25 @@
-﻿CREATE VIEW dbo.GeneralJournalItem_View
+﻿
+CREATE VIEW [dbo].[GeneralJournalItem_View]
 AS
 SELECT        dbo.GeneralJournalItem.IdNo, dbo.GeneralJournalItem.Sequence, dbo.GeneralJournalItem.JournalIdNo, dbo.GeneralJournalItem.AccountIdNo, dbo.GeneralJournalItem.Debit, dbo.GeneralJournalItem.Credit, 
                          dbo.GeneralJournalItem.RevCostCenterIdNo, dbo.GeneralJournalItem.Notes, dbo.Account.AccountName, dbo.GeneralJournalItem.Debit - dbo.GeneralJournalItem.Credit AS OriginalAmount, dbo.Account.PayeeType, 
-                         dbo.Account.SpecialAccount, 0 AS OpenInvoiceIdNo, 0 AS PaidAmount, dbo.ApOpenInvoice.PaidAmount AS Expr1, dbo.ApOpenInvoice.DiscountTaken, dbo.GeneralJournalItem.PayIdNo
-FROM            dbo.GeneralJournalItem LEFT OUTER JOIN
-                         dbo.Account ON dbo.GeneralJournalItem.AccountIdNo = dbo.Account.IdNo LEFT OUTER JOIN
-                         dbo.ApOpenInvoice ON dbo.GeneralJournalItem.JournalIdNo = dbo.ApOpenInvoice.JournalItemIdNo AND dbo.ApOpenInvoice.JournalCode = 'GJ'
+                         dbo.Account.SpecialAccount, 0 AS OpenInvoiceIdNo, 0 AS PaidAmount, dbo.ApOpenInvoice.PaidAmount AS Expr1, dbo.ApOpenInvoice.DiscountTaken, dbo.GeneralJournalItem.PayIdNo,
+						 IIf(dbo.Account.SpecialAccount='AR',dbo.Customer.CustomerCode,IIf(dbo.Account.SpecialAccount='AP',dbo.Supplier.SupplierCode, iiF(dbo.Account.SpecialAccount='EL',dbo.Employee.EmployeeCode,''))) as PayCode,
+						 IIf(dbo.Account.SpecialAccount='AR',dbo.Customer.CustomerName,IIf(dbo.Account.SpecialAccount='AP',dbo.Supplier.SupplierName, iiF(dbo.Account.SpecialAccount='EL',dbo.Employee.EmployeeName,''))) as PayName,
+						 IIf(dbo.Account.SpecialAccount='AR',dbo.Customer.CustomerNameAra,IIf(dbo.Account.SpecialAccount='AP',dbo.Supplier.SupplierNameAra, iiF(dbo.Account.SpecialAccount='EL',dbo.Employee.EmployeeNameAra,''))) as PayNameAra
+FROM         dbo.GeneralJournalItem 
+		     Left Join dbo.Account ON dbo.GeneralJournalItem.AccountIdNo = dbo.Account.IdNo 
+			 Left Join dbo.Customer ON dbo.GeneralJournalItem.PayIdNo = dbo.Customer.IdNo 
+			 Left Join dbo.Supplier ON dbo.GeneralJournalItem.PayIdNo = dbo.Supplier.IdNo 
+			 Left Join dbo.Employee ON dbo.GeneralJournalItem.PayIdNo = dbo.Employee.IdNo 
+			 Left Join dbo.ApOpenInvoice ON dbo.GeneralJournalItem.JournalIdNo = dbo.ApOpenInvoice.JournalItemIdNo AND dbo.ApOpenInvoice.JournalCode = 'GJ'
 
 GO
-EXECUTE sp_addextendedproperty @name = N'MS_DiagramPaneCount', @value = 1, @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'VIEW', @level1name = N'GeneralJournalItem_View';
+
 
 
 GO
-EXECUTE sp_addextendedproperty @name = N'MS_DiagramPane1', @value = N'[0E232FF0-B466-11cf-A24F-00AA00A3EFFF, 1.00]
-Begin DesignProperties = 
-   Begin PaneConfigurations = 
-      Begin PaneConfiguration = 0
-         NumPanes = 4
-         Configuration = "(H (1[40] 4[20] 2[20] 3) )"
-      End
-      Begin PaneConfiguration = 1
-         NumPanes = 3
-         Configuration = "(H (1 [50] 4 [25] 3))"
-      End
-      Begin PaneConfiguration = 2
-         NumPanes = 3
-         Configuration = "(H (1 [50] 2 [25] 3))"
-      End
-      Begin PaneConfiguration = 3
-         NumPanes = 3
-         Configuration = "(H (4 [30] 2 [40] 3))"
-      End
-      Begin PaneConfiguration = 4
-         NumPanes = 2
-         Configuration = "(H (1 [56] 3))"
-      End
-      Begin PaneConfiguration = 5
-         NumPanes = 2
-         Configuration = "(H (2 [66] 3))"
-      End
-      Begin PaneConfiguration = 6
-         NumPanes = 2
-         Configuration = "(H (4 [50] 3))"
-      End
-      Begin PaneConfiguration = 7
-         NumPanes = 1
-         Configuration = "(V (3))"
-      End
-      Begin PaneConfiguration = 8
-         NumPanes = 3
-         Configuration = "(H (1[56] 4[18] 2) )"
-      End
-      Begin PaneConfiguration = 9
-         NumPanes = 2
-         Configuration = "(H (1 [75] 4))"
-      End
-      Begin PaneConfiguration = 10
-         NumPanes = 2
-         Configuration = "(H (1[66] 2) )"
-      End
-      Begin PaneConfiguration = 11
-         NumPanes = 2
-         Configuration = "(H (4 [60] 2))"
-      End
-      Begin PaneConfiguration = 12
-         NumPanes = 1
-         Configuration = "(H (1) )"
-      End
-      Begin PaneConfiguration = 13
-         NumPanes = 1
-         Configuration = "(V (4))"
-      End
-      Begin PaneConfiguration = 14
-         NumPanes = 1
-         Configuration = "(V (2))"
-      End
-      ActivePaneConfig = 0
-   End
-   Begin DiagramPane = 
-      Begin Origin = 
-         Top = 0
-         Left = 0
-      End
-      Begin Tables = 
-         Begin Table = "GeneralJournalItem"
-            Begin Extent = 
-               Top = 6
-               Left = 38
-               Bottom = 335
-               Right = 231
-            End
-            DisplayFlags = 280
-            TopColumn = 0
-         End
-         Begin Table = "Account"
-            Begin Extent = 
-               Top = 138
-               Left = 38
-               Bottom = 268
-               Right = 237
-            End
-            DisplayFlags = 280
-            TopColumn = 0
-         End
-         Begin Table = "ApOpenInvoice"
-            Begin Extent = 
-               Top = 6
-               Left = 269
-               Bottom = 136
-               Right = 446
-            End
-            DisplayFlags = 280
-            TopColumn = 0
-         End
-      End
-   End
-   Begin SQLPane = 
-   End
-   Begin DataPane = 
-      Begin ParameterDefaults = ""
-      End
-   End
-   Begin CriteriaPane = 
-      Begin ColumnWidths = 11
-         Column = 1440
-         Alias = 900
-         Table = 1170
-         Output = 720
-         Append = 1400
-         NewValue = 1170
-         SortType = 1350
-         SortOrder = 1410
-         GroupBy = 1350
-         Filter = 1350
-         Or = 1350
-         Or = 1350
-         Or = 1350
-      End
-   End
-End
-', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'VIEW', @level1name = N'GeneralJournalItem_View';
+
 
 
 
