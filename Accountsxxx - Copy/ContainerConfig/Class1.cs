18141aa907aed0c0b803e196340765d91fe148a6
Arnel Marcelo <arnelmarcelo@hotmail.com>
2022-08-20 17:04:03 +0300

    delete backups

--- Accountsxxx - Copy/ContainerConfig/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace ContainerConfig
-{
-    public class Class1
-    {
-    }
-}
--- Accountsxxx Copy Orig/PresentationLayer/Models/MainModel.vb
+++
@@ -1,8 +0,0 @@
-﻿Namespace PresentationLayer.Models
-
-    Public Class MainModel
-        Public Property IdNo As Int32
-        Public Property UserName As String
-    End Class
-
-End Namespace
