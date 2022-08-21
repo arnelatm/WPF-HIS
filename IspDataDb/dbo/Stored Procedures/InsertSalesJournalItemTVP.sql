3a237bd10f6feb72e1a1926f6083c103f8bdb6ca
Arnel Marcelo <arnelmarcelo@hotmail.com>
2022-08-21 11:50:12 +0300

    Updated Tables & Database

--- IspDataDb/dbo/Stored Procedures/InsertSalesJournalItemTVP.sql
+++ IspDataDb/dbo/Stored Procedures/InsertSalesJournalItemTVP.sql
@@ -6,11 +6,12 @@
 
 
 
+
 CREATE PROC [dbo].[InsertSalesJournalItemTVP]
   @MParam JournalItemInsert READONLY
 AS 
-INSERT  INTO SalesJournalItem (AccountIdNo, Credit, Debit, JournalIdNo, Notes, RevCostCenterIdNo, Sequence)
-        SELECT  AccountIdNo,Credit, Debit, JournalIdNo, Notes, RevCostCenteridNo, Sequence
+INSERT  INTO SalesJournalItem (AccountIdNo, Credit, Debit, JournalIdNo, Notes, PayIdNo, RevCostCenterIdNo, Sequence)
+        SELECT  AccountIdNo,Credit, Debit, JournalIdNo, Notes, PayIdNo, RevCostCenteridNo, Sequence
         FROM    @MParam
 SET IDENTITY_INSERT DBO.SalesJournalItem ON;
 
