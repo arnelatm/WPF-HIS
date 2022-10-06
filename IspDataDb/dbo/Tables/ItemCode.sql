feca66567d50f672da7fb188680b4b248b27c058
Arnel Marcelo <arnelmarcelo@hotmail.com>
2022-10-06 22:22:49 +0300

    Updated Databases

---
+++ IspDataDb/dbo/Tables/ItemCode.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[ItemCode] (
+    [IdNo]            INT           NULL,
+    [ItemCodeName]    NVARCHAR (50) NULL,
+    [ItemCodeNameAra] NVARCHAR (50) NULL,
+    [ItemCodeCode]    NVARCHAR (5)  NULL,
+    [CodeGroupIdNo]   TINYINT       NULL,
+    [DateTimeStamp]   ROWVERSION    NULL
+);
+
