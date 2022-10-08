f47d2c4c2929c3641d3b6198cb86a1f5e432f722
Arnel Marcelo <arnelmarcelo@hotmail.com>
2022-10-08 11:38:32 +0300

    Editing

--- PmrDoctorsGenForm_View.sql
+++ PmrDoctorsGenForm_View.sql
@@ -1,4 +1,15 @@
-﻿ALTER view 	[dbo].[PMRDoctorsGenForm_View]
+﻿USE [iGroupClinic]
+GO
+
+/****** Object:  View [dbo].[PMRDoctorsGenForm_View]    Script Date: 08/10/2022 8:59:35 am ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+ALTER view 	[dbo].[PMRDoctorsGenForm_View]
  
 as 
 SELECT a.DoctorId,
@@ -10,11 +21,12 @@ SELECT a.DoctorId,
 	Cast(a.TokenNo as Int) as Token, 
 	IIf(Cast(a.TokenNo as Int)=0,0,1) as Status,
 	b.LastConsDate,
-	a.Trans_Key,
-	Min(c.Create_Date) as InvTime
+	f.Trans_Key,
+	(SELECT MIN( g.Create_Date)
+              FROM ClinicInvoiceGroup g
+              WHERE a.RegistrationNo = g.RegistrationNo and a.DoctorId = g.DoctorId and a.RegistrationNo = g.RegistrationNo and a.PatientType = g.RegistrationType and a.TransDateEnglish = g.TransDateEnglish) as InvTime
 FROM PMRPatientGeneralInfo a 
 left outer join PatientDetails b on a.Series = b.Series AND a.RegistrationNo = b.RegistrationNo
 left outer join EmployeeDetails e on a.DoctorID = e.EmpID
-left outer join ClinicInvoiceGroup c on c.TransDateEnglish = a.TransDateEnglish and c.RegistrationNo = a.RegistrationNo and c.TransType = a.TransType 
-group by a.Trans_Key,a.DoctorId,a.TokenNo,a.TransDateEnglish,a.RegistrationNo,a.TransType,a.PatientType,b.PatientNameEnglish,b.RegistrationDate,b.LastConsDate
+left outer join PMRTokenDetails f on f.PMRDateEnglish = a.TransDateEnglish AND a.DoctorID = f.DoctorID AND a.TokenNo = f.TokenNo 
 GO
