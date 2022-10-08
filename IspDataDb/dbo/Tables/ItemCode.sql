8bd50c5249571cc9338f15ee08e3ecdd7d84ae1e
Arnel Marcelo <arnelmarcelo@hotmail.com>
2022-10-08 19:04:41 +0300

    Added Doctor Table

--- IspDataDb/dbo/Tables/ItemCode.sql
+++ IspDataDb/dbo/Tables/ItemCode.sql
@@ -1,9 +1,12 @@
 ﻿CREATE TABLE [dbo].[ItemCode] (
-    [IdNo]            INT           NULL,
+    [IdNo]            INT           IDENTITY (1, 1) NOT NULL,
     [ItemCodeName]    NVARCHAR (50) NULL,
     [ItemCodeNameAra] NVARCHAR (50) NULL,
     [ItemCodeCode]    NVARCHAR (5)  NULL,
     [CodeGroupIdNo]   TINYINT       NULL,
-    [DateTimeStamp]   ROWVERSION    NULL
+    [DateTimeStamp]   ROWVERSION    NULL,
+    CONSTRAINT [PK_ItemCode] PRIMARY KEY CLUSTERED ([IdNo] ASC)
 );
+
+
 
