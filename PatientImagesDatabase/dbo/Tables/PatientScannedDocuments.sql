398c2bc2bd4d81c6f0181ca17638f11266c684e5
Arnel Marcelo <arnelmarcelo@hotmail.com>
2022-10-11 18:56:17 +0300

    Addded PaTIENTiMAGE DATABASE

---
+++ PatientImagesDatabase/dbo/Tables/PatientScannedDocuments.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[PatientScannedDocuments] (
+    [BranchID]       VARCHAR (15)   NOT NULL,
+    [RegistrationNo] NUMERIC (12)   NOT NULL,
+    [PatientType]    VARCHAR (15)   NOT NULL,
+    [DocumentID]     VARCHAR (15)   NOT NULL,
+    [DocumentType]   VARCHAR (15)   NULL,
+    [Width]          NUMERIC (7, 2) NULL,
+    [Height]         NUMERIC (7, 2) NULL,
+    [Brightness]     NUMERIC (2)    NULL,
+    [Contrast]       NUMERIC (2)    NULL,
+    [DocumentInfo]   IMAGE          NULL,
+    [UserID]         VARCHAR (15)   NULL,
+    [Create_Date]    DATETIME       NULL,
+    [MachineID]      VARCHAR (20)   NULL
+);
+
