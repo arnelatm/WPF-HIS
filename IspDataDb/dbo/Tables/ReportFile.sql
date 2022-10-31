5ae97026fae89aa91c8134a6d90aa8a410110458
Arnel Marcelo <arnelmarcelo@hotmail.com>
2022-10-31 17:06:38 +0300

    Editing

---
+++ IspDataDb/dbo/Tables/ReportFile.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[ReportFile] (
+    [IdNo]           SMALLINT       IDENTITY (1, 1) NOT NULL,
+    [ReportFileName] VARCHAR (200)  NULL,
+    [ReportTitle]    VARCHAR (200)  NULL,
+    [ReportTitleAra] NVARCHAR (200) NULL,
+    [PrintSetupIdNo] SMALLINT       NULL,
+    [Active]         BIT            NULL,
+    [DateCreated]    DATE           CONSTRAINT [DF__Report__Creat__2E7BCEF5] DEFAULT (getdate()) NULL,
+    CONSTRAINT [PK_Report] PRIMARY KEY CLUSTERED ([IdNo] ASC)
+);
+
