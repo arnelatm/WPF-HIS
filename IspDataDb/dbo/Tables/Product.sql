dde8bff12bff9914d51dbb8929fbbcf3690861e1
Arnel Marcelo <arnelmarcelo@hotmail.com>
2023-03-22 15:24:16 +0300

    Editing

--- IspDataDb/dbo/Tables/Product.sql
+++ IspDataDb/dbo/Tables/Product.sql
@@ -1,14 +1,18 @@
 ﻿CREATE TABLE [dbo].[Product] (
     [IdNo]           INT            IDENTITY (1, 1) NOT NULL,
     [CategoryIdNo]   SMALLINT       NULL,
-    [Code]           VARCHAR (10)   COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
-    [Name]           VARCHAR (50)   COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
-    [NameAra]        NVARCHAR (100) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-    [BaseUnitIdNo]   TINYINT       NULL,
+    [ProductCode]    VARCHAR (10)   COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+    [ProductName]    VARCHAR (50)   COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+    [ProductNameAra] NVARCHAR (100) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+    [Barcode]        VARCHAR (13)   NULL,
+    [GTIN]           VARCHAR (14)   NULL,
+    [BaseUnitIdNo]   SMALLINT       NULL,
     [Active]         BIT            NULL,
     [DateCreated]    DATE           CONSTRAINT [DF_Product_DateCreated] DEFAULT (getdate()) NULL,
     [DateTimeStamp]  ROWVERSION     NULL,
     [CreatedByIdNo]  SMALLINT       NULL,
     CONSTRAINT [PK_Product] PRIMARY KEY CLUSTERED ([IdNo] ASC)
 );
+
+
 
--- IspDataDb/dbo/Tables/Unit.sql
+++ IspDataDb/dbo/Tables/Unit.sql
@@ -1,8 +1,10 @@
 ﻿CREATE TABLE [dbo].[Unit] (
     [IdNo]        SMALLINT      IDENTITY (1, 1) NOT NULL,
-    [Code]        NVARCHAR (10) NULL,
+    [UnitCode]    NVARCHAR (10) NULL,
     [UnitName]    VARCHAR (50)  NULL,
     [UnitNameAra] NVARCHAR (50) NULL,
     CONSTRAINT [PK_Unit] PRIMARY KEY CLUSTERED ([IdNo] ASC)
 );
+
+
 
