0726463637935b59031a5eb268d97387879dae23
Arnel Marcelo <arnelmarcelo@hotmail.com>
2023-03-22 15:24:16 +0300

    Editing

---
+++ IspDataDb/dbo/Tables/PurchaseDetail.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[PurchaseDetail] (
+    [IdNo]           INT            NULL,
+    [Sequence]       SMALLINT       NULL,
+    [PurchaseIdNo]   INT            NULL,
+    [ProductIdNo]    INT            NULL,
+    [Quantity]       SMALLINT       NULL,
+    [BonusQuantity]  INT            NULL,
+    [UnitIdNo]       TINYINT        NULL,
+    [Price]          SMALLMONEY     NULL,
+    [DiscountAmount] MONEY          NULL,
+    [VatPercent]     DECIMAL (5, 2) NULL,
+    [VatAmount]      SMALLMONEY     NULL,
+    [NetAmount]      MONEY          NULL
+);
+
