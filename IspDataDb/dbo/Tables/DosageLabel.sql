725c9b553d03423f4176bd5114acd9b1c93ed0a8
Arnel Marcelo <arnelmarcelo@hotmail.com>
2023-06-22 22:49:00 +0300

    Editing

--- IspDataDb/dbo/Tables/DosageLabel.sql
+++ IspDataDb/dbo/Tables/DosageLabel.sql
@@ -1,12 +1,16 @@
 ﻿CREATE TABLE [dbo].[DosageLabel] (
-    [IdNo]         INT          NULL,
+    [IdNo]         INT          IDENTITY (1, 1) NOT NULL,
     [ComputerName] VARCHAR (50) NULL,
     [PatientName]  VARCHAR (50) NULL,
+    [FileNo]       INT          NULL,
     [Age]          VARCHAR (5)  NULL,
     [AgeYMD]       CHAR (1)     NULL,
     [Gender]       CHAR (1)     NULL,
-    [DoctorsName]  VARCHAR (50) NULL
+    [DoctorsName]  VARCHAR (50) NULL,
+    CONSTRAINT [PK_DosageLabel] PRIMARY KEY CLUSTERED ([IdNo] ASC)
 );
+
+
 
 
 
