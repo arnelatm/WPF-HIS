fedf2dad2b8f6fb4817575b12b30afecf91b0798
Arnel Marcelo <arnelmarcelo@hotmail.com>
2023-07-29 16:41:38 +0300

    Editing

--- Accounts/DataLayer/AdoNet/PurchaseDao.vb
+++ Accounts/DataLayer/AdoNet/PurchaseDao.vb
@@ -125,10 +125,14 @@ Namespace DataLayer.AdoNet
 
             Dim retVal As Boolean
             Dim commands As New List(Of DaoCommand)
-            Dim command1,command2 As New DaoCommand
-            command1.Add("Insert into Inventory (PurchaseDetailIdNo,ProductIdNo,QtyOnHand) " &
-                        "select IdNo,ProductIdNo,Quantity+BonusQuantity from PurchaseDetail " &
-                        "where PurchaseIdNo = @IdNo", {"@IdNo", idNo})
+            Dim command1, command2 As New DaoCommand
+            command1.Add("Select Case a.IdNo,a.ProductIdNo,(a.Quantity+a.BonusQuantity) * c.BaseQty / c.UnitQty,b.WarehouseIdNo " &
+                         "From PurchaseDetail a Left Join Purchase b On a.PurchaseIdNo = b.IdNo " &
+                         "Left Join ProductUnit_View c On a.ProductIdNo = c.ProductIdNo And a.UnitIdNo = c.UnitIdNo ", {"@IdNo", idNo})
+            'command1.Add("Insert into Inventory (PurchaseDetailIdNo,ProductIdNo,QtyOnHand,WarehouseIdNo) " &
+            '            "select a.IdNo,a.ProductIdNo,a.Quantity+a.BonusQuantity,b.WarehouseIdNo from PurchaseDetail a " &
+            '            "left join Purchase b on a.PurchaseIdNo = b.IdNo " &
+            '            "where a.PurchaseIdNo = @IdNo", {"@IdNo", idNo})
             commands.Add(command1)
             command2.Add("Update Purchase set Posted = 1 where IdNo = @IdNo", {"@IdNo", idNo})
             commands.Add(command2)
