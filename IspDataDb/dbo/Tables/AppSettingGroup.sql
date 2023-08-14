6dac794659964440b14b1e5f17096e283ee1c672
Arnel Marcelo <arnelmarcelo@hotmail.com>
2023-08-14 05:17:57 +0300

    DB update

---
+++ IspDataDb/dbo/Tables/AppSettingGroup.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[AppSettingGroup] (
+    [IdNo]                   SMALLINT      IDENTITY (1, 1) NOT NULL,
+    [AppSettingCode]         CHAR (4)      NULL,
+    [AppSettingGroupName]    VARCHAR (50)  NULL,
+    [AppSettingGroupNameAra] NVARCHAR (50) NULL,
+    [SelectorTable1]         VARCHAR (50)  NULL,
+    [SelectorTable2]         VARCHAR (50)  NULL,
+    [SelectorText1]          VARCHAR (20)  NULL,
+    [SelectorText2]          VARCHAR (20)  NULL,
+    [SelectorCount]          TINYINT       NULL,
+    CONSTRAINT [PK_InventorySettings] PRIMARY KEY CLUSTERED ([IdNo] ASC)
+);
+
