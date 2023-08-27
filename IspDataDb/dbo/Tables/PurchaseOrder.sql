5fabd7b96d484de1f7ded26c42bf53005fd4b39c
arnelaatm <arnel.aatm@yahoo.com>
2023-08-27 12:23:10 +0300

    Updated Tables

--- IspDataDb/dbo/Tables/PurchaseOrder.sql
+++ IspDataDb/dbo/Tables/PurchaseOrder.sql
@@ -7,11 +7,18 @@
     [WarehouseIdNo]   SMALLINT       NOT NULL,
     [Amount]          DECIMAL (9, 2) NULL,
     [Cancelled]       BIT            NULL,
-    [Notes]           NVARCHAR (100) NOT NULL,
+    [Notes]           NVARCHAR (100) NULL,
     [Posted]          BIT            NULL,
     [DateCreated]     DATE           CONSTRAINT [DF_PurchaseOrder_DateCreated] DEFAULT (getdate()) NULL,
     [UserIdNo]        SMALLINT       NOT NULL,
     [DateTimeStamp]   ROWVERSION     NULL,
     CONSTRAINT [PK_PurchaseOrder] PRIMARY KEY CLUSTERED ([IdNo] ASC)
 );
+
+
+
+
+GO
+CREATE UNIQUE NONCLUSTERED INDEX [IX_PurchaseOrder]
+    ON [dbo].[PurchaseOrder]([ReferenceNo] ASC);
 
---
+++ IspDataDb/dbo/Views/PurchaseOrderDetail_View.sql
@@ -0,0 +1,20 @@
+﻿
+
+
+
+
+
+
+
+
+
+CREATE VIEW [dbo].[PurchaseOrderDetail_View]
+AS
+SELECT        dbo.PurchaseOrderDetail.IdNo, dbo.PurchaseOrderDetail.Sequence, dbo.PurchaseOrderDetail.PurchaseOrderIdNo, dbo.PurchaseOrderDetail.ProductIdNo, dbo.PurchaseOrderDetail.Quantity, dbo.PurchaseOrderDetail.UnitIdNo, 
+                         dbo.PurchaseOrderDetail.NetAmount, dbo.Product.ProductCode, dbo.Product.ProductName,dbo.Product.ProductNameAra, dbo.Product.Barcode, dbo.Product.GTIN, dbo.Product.BaseUnitIdNo, dbo.Unit.UnitCode, dbo.Unit.UnitName, dbo.Unit.UnitNameAra, dbo.Category.VatSaleAccountIdNo, 
+                         dbo.Category.SaleAccountIdNo, dbo.Category.PurchaseAccountIdNo, dbo.Product.CategoryIdNo, (select count(dbo.ProductUnit.ProductIdNo) from dbo.ProductUnit where dbo.ProductUnit.ProductIdNo = dbo.PurchaseOrderDetail.ProductIdNo) as UnitCount,
+						 dbo.Category.NeedsExpiryDate, dbo.PurchaseOrderDetail.UnitCost
+FROM            dbo.PurchaseOrderDetail LEFT OUTER JOIN
+                         dbo.Unit ON dbo.PurchaseOrderDetail.UnitIdNo = dbo.Unit.IdNo LEFT OUTER JOIN
+                         dbo.Product ON dbo.PurchaseOrderDetail.ProductIdNo = dbo.Product.IdNo LEFT OUTER JOIN
+                         dbo.Category ON dbo.Product.CategoryIdNo = dbo.Category.IdNo
