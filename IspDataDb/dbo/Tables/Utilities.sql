babea7c4b7ce24f1d572cd40df7e457a5dbd4e0a
arnelaatm <arnel.aatm@yahoo.com>
2023-09-12 13:46:41 +0300

    editing

---
+++ IspDataDb/dbo/Tables/Utilities.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Utilities] (
+    [IdNo]            SMALLINT     IDENTITY (1, 1) NOT NULL,
+    [UtilityName]     VARCHAR (30) NULL,
+    [StoredProcedure] BIT          NULL,
+    CONSTRAINT [PK_Utilities] PRIMARY KEY CLUSTERED ([IdNo] ASC)
+);
+
+
+GO
+CREATE UNIQUE NONCLUSTERED INDEX [IX_Utilities]
+    ON [dbo].[Utilities]([UtilityName] ASC);
+
