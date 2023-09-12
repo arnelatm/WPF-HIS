78e6c1d82a6eabc12b71c4cc9163c42b97e1edd3
Arnel Marcelo <arnelmarcelo@hotmail.com>
2023-09-12 22:08:35 +0300

    Edting

---
+++ IspDataDb/dbo/Views/InventoryCount_View.sql
@@ -0,0 +1,16 @@
+﻿
+
+
+/****** Script for SelectTopNRows command from SSMS  ******/
+CREATE VIEW [dbo].[InventoryCount_View]
+as
+SELECT [BranchIdNo]
+      ,[ProductIdNo]
+      ,sUM([QtyOnHand]) as QtyOnHand
+      ,[ProductCode]
+      ,[ProductName]
+      ,[WarehouseIdNo]
+      ,iif(sum([QtyOnHand])=0,0,sUM([TotalCost])/Sum([QtyOnHand])) as UnitCost
+      ,sUM([TotalCost]) AS TotalCost
+  FROM [ISPData].[dbo].[Inventory_View]
+  GROUP BY BRANCHIDNO,PRODUCTIDNO,PRODUCTCODE,ProductName,WAREHOUSEIDNO
