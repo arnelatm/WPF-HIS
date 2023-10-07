f1155c2ac8c6e47475824c197e027a5b72fccf0a
Arnel Marcelo <arnelmarcelo@hotmail.com>
2023-10-07 14:39:59 +0300

    Editing

---
+++ IspDataDb/dbo/Views/ItemInventoryDetailsQty_View.sql
@@ -0,0 +1,5 @@
+﻿Create VIEW ItemInventoryDetailsQty_View as 
+Select a.IdNo,a.ProductIdNo,IIf(c.UnitQty=0,0,Cast(a.Quantity as Decimal(12,2)) * c.BaseQty / c.UnitQty) as Quantity,
+       IIf((cast(a.Quantity as Decimal(12,2)) * c.BaseQty / c.UnitQty)=0,0,a.NetAmount / (cast(a.Quantity as Decimal(12,2)) * c.BaseQty / c.UnitQty)) as UnitCost, a.NetAmount , a.BatchNo, a.ExpiryDate 
+       From InvTransactionDetail a Left Join InvTransaction b On a.InvTransactionIdNo = b.IdNo 
+       Left Join ProductUnit_View c On a.ProductIdNo = c.ProductIdNo And a.UnitIdNo = c.UnitIdNo
--- IspDataDb/dbo/Views/PurchaseOrderApprovalDetail_View.sql
+++ IspDataDb/dbo/Views/PurchaseOrderApprovalDetail_View.sql
@@ -9,6 +9,7 @@
 
 
 
+
 CREATE VIEW [dbo].[PurchaseOrderApprovalDetail_View]
 AS
 SELECT        dbo.PurchaseOrderDetail.IdNo, dbo.PurchaseOrderDetail.Sequence, dbo.PurchaseOrderDetail.PurchaseOrderIdNo, dbo.PurchaseOrderDetail.ProductIdNo, dbo.PurchaseOrderDetail.Quantity, dbo.PurchaseOrderDetail.BonusQuantity, dbo.PurchaseOrderDetail.UnitIdNo, 
@@ -16,8 +17,12 @@ SELECT        dbo.PurchaseOrderDetail.IdNo, dbo.PurchaseOrderDetail.Sequence, db
                          dbo.Product.ProductNameAra, dbo.Product.Barcode, dbo.Product.GTIN, dbo.Product.BaseUnitIdNo, dbo.Unit.UnitCode, dbo.Unit.UnitName, dbo.Unit.UnitNameAra, dbo.Category.VatSaleAccountIdNo, 
                          dbo.Category.VatPurchaseAccountIdNo, dbo.Category.VatPercentage, dbo.Category.SaleAccountIdNo, dbo.Category.PurchaseAccountIdNo, dbo.Product.CategoryIdNo, (select count(dbo.ProductUnit.ProductIdNo) from dbo.ProductUnit where dbo.ProductUnit.ProductIdNo = dbo.PurchaseOrderDetail.ProductIdNo) as UnitCount,
 						 dbo.PurchaseOrderDetail.Quantity * dbo.PurchaseOrderDetail.Price as 'GrossAmount', IIf(dbo.PurchaseOrderDetail.Quantity * dbo.PurchaseOrderDetail.Price = 0,0, dbo.PurchaseOrderDetail.DiscountAmount / (dbo.PurchaseOrderDetail.Quantity * dbo.PurchaseOrderDetail.Price) * 100) as 'DiscountPercent',
-						 IIf(dbo.PurchaseOrderDetail.Quantity * dbo.PurchaseOrderDetail.Price = 0,0, dbo.PurchaseOrderDetail.Quantity * dbo.PurchaseOrderDetail.Price) - dbo.PurchaseOrderDetail.DiscountAmount as 'AmtBefVat'
-FROM            dbo.PurchaseOrderDetail LEFT OUTER JOIN
-                         dbo.Unit ON dbo.PurchaseOrderDetail.UnitIdNo = dbo.Unit.IdNo LEFT OUTER JOIN
-                         dbo.Product ON dbo.PurchaseOrderDetail.ProductIdNo = dbo.Product.IdNo LEFT OUTER JOIN
-                         dbo.Category ON dbo.Product.CategoryIdNo = dbo.Category.IdNo
+						 IIf(dbo.PurchaseOrderDetail.Quantity * dbo.PurchaseOrderDetail.Price = 0,0, dbo.PurchaseOrderDetail.Quantity * dbo.PurchaseOrderDetail.Price) - dbo.PurchaseOrderDetail.DiscountAmount as 'AmtBefVat',
+						 dbo.ProductUnit_View.UnitName as 'BaseUnitName', dbo.InventoryCount_View.QtyOnHand as 'QtyOnHand', dbo.InventoryCount_View.UnitCost, dbo.PurchaseOrderSupplied.QtySupplied as 'QtySupplied'
+FROM            dbo.PurchaseOrderDetail 
+				LEFT OUTER JOIN dbo.Unit ON dbo.PurchaseOrderDetail.UnitIdNo = dbo.Unit.IdNo 
+				LEFT OUTER JOIN dbo.Product ON dbo.PurchaseOrderDetail.ProductIdNo = dbo.Product.IdNo 
+				LEFT OUTER JOIN dbo.Category ON dbo.Product.CategoryIdNo = dbo.Category.IdNo
+				LEFT OUTER JOIN dbo.ProductUnit_View on dbo.Product.BaseUnitIdNo = dbo.ProductUnit_View.UnitIdNo and dbo.ProductUnit_View.ProductIdNo = dbo.Product.IdNo
+				LEFT OUTER JOIN dbo.InventoryCount_View on dbo.PurchaseOrderDetail.ProductIdNo = dbo.InventoryCount_View.ProductIdNo
+				LEFT OUTER JOIN dbo.PurchaseOrderSupplied on dbo.PurchaseOrderDetail.IdNo = dbo.PurchaseOrderSupplied.PurchaseOrderDetailIdNo
