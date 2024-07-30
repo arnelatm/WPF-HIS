2a87c150e3a724db76411975e4c2b2ddc2109ed7
arneatm <arnelmarcelo@yahoo.com>
2024-07-30 16:59:43 +0300

    Editing

--- IspDataDb/dbo/User Defined Types/JournalItemUpdate.sql
+++ IspDataDb/dbo/User Defined Types/JournalItemUpdate.sql
@@ -6,10 +6,11 @@
     [IDNo]              INT            NOT NULL,
     [JournalIDNo]       INT            NOT NULL,
     [Notes]             NVARCHAR (300) NULL,
-    [PayIdNo]           INT            NULL,
     [RevCostCenterIdNo] INT            NULL,
     [Sequence]          INT            NOT NULL,
     PRIMARY KEY CLUSTERED ([IDNo] ASC));
+
+
 
 
 
