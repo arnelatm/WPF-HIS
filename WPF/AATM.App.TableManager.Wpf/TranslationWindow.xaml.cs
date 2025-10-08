4d658f4c4a23bb0223a408c2ee39f9bf3e27bbc8
arneatm <arnelmarcelo@yahoo.com>
2025-10-08 19:22:10 +0300

    Created a new WPF Project

---
+++ WPF/AATM.App.TableManager.Wpf/TranslationWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AATM.App.TableManager.Wpf
+{
+    /// <summary>
+    /// Interaction logic for TranslationWindow.xaml
+    /// </summary>
+    public partial class TranslationWindow : Window
+    {
+        public TranslationWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ WPF/App/AATM.App.Wpf.HIS/ViewModels/TranslationViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using AATM.Contracts.Dtos; // Reference your DTO
+
+namespace AATM.App.Wpf.HIS.ViewModels
+{
+    public class TranslationViewModel : INotifyPropertyChanged
+    {
+        public ObservableCollection<TranslationDto> Translations { get; } = new();
+        private int _currentIndex;
+        public int CurrentIndex
+        {
+            get => _currentIndex;
+            set { _currentIndex = value; OnPropertyChanged(); OnPropertyChanged(nameof(CurrentTranslation)); }
+        }
+        public TranslationDto CurrentTranslation =>
+            (Translations.Count > 0 && CurrentIndex >= 0 && CurrentIndex < Translations.Count) 
+            ? Translations[CurrentIndex] : null;
+
+        // Navigation commands (same as before)
+        public ICommand FirstCommand { get; }
+        public ICommand PreviousCommand { get; }
+        public ICommand NextCommand { get; }
+        public ICommand LastCommand { get; }
+
+        public TranslationViewModel()
+        {
+            // TODO: Load data using your existing WinForms logic/services
+            // Example: Translations.AddRange(TranslationDbService.GetAll());
+
+            FirstCommand = new RelayCommand(_ => CurrentIndex = 0, _ => Translations.Count > 0 && CurrentIndex > 0);
+            PreviousCommand = new RelayCommand(_ => { if (CurrentIndex > 0) CurrentIndex--; }, _ => CurrentIndex > 0);
+            NextCommand = new RelayCommand(_ => { if (CurrentIndex < Translations.Count - 1) CurrentIndex++; }, _ => CurrentIndex < Translations.Count - 1);
+            LastCommand = new RelayCommand(_ => CurrentIndex = Translations.Count - 1, _ => Translations.Count > 0 && CurrentIndex < Translations.Count - 1);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string name = null) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+
+}
