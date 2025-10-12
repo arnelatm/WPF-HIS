b9b9ad56fed21773168e7dcfaf0b9d9cc210b871
arneatm <arnelmarcelo@yahoo.com>
2025-10-12 16:32:50 +0300

    Move assembly metadata from AssemblyInfo.cs to .csproj
    Assembly attributes such as title, product, copyright, and version were removed from AssemblyInfo.cs and added to the AATM.Core.csproj file. This centralizes assembly metadata management in the project file, streamlining configuration and reducing redundancy.

--- Core/My Project/AssemblyInfo.cs
+++ Core/My Project/AssemblyInfo.cs
@@ -1,32 +1,9 @@
 ﻿using System;
 using System.Reflection;
 using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-
-// Review the values of the assembly attributes
-
-[assembly: AssemblyTitle("Core")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Core")]
-[assembly: AssemblyCopyright("Copyright ©  2025")]
 [assembly: AssemblyTrademark("")]
 
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("eda95117-b38e-41de-b2fc-49de56929965")]
-
-// Version information for an assembly consists of the following four values:
-// 
-// Major Version
-// Minor Version
-// Build Number
-// Revision
-// 
-
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
